74c03cec4549738bb1fc20fd881fbd9c750f6928
Bridget Lane <bnlane@gannett.com>
2017-10-05 13:43:53 -0400

    Add OWASP-functionality

--- fastly/waf.go
+++ fastly/waf.go
@@ -3,7 +3,6 @@ package fastly
 import (
 	"fmt"
 	"reflect"
-	"time"
 
 	"github.com/google/jsonapi"
 )
@@ -15,11 +14,11 @@ type WAFConfigurationSet struct {
 
 // WAF is the information about a firewall object.
 type WAF struct {
-	ID                string     `jsonapi:"primary,waf"`
-	Version           int        `jsonapi:"attr,version"`
-	PrefetchCondition string     `jsonapi:"attr,prefetch_condition"`
-	Response          string     `jsonapi:"attr,response"`
-	LastPush          *time.Time `jsonapi:"attr,last_push"`
+	ID                string `jsonapi:"primary,waf"`
+	Version           int    `jsonapi:"attr,version"`
+	PrefetchCondition string `jsonapi:"attr,prefetch_condition"`
+	Response          string `jsonapi:"attr,response"`
+	LastPush          string `jsonapi:"attr,last_push"`
 
 	ConfigurationSet *WAFConfigurationSet `jsonapi:"relation,configuration_set"`
 }
@@ -180,7 +179,7 @@ func (c *Client) UpdateWAF(i *UpdateWAFInput) (*WAF, error) {
 	return &waf, nil
 }
 
-// DeleteWAFInput is used as input to the GetWAF function.
+// DeleteWAFInput is used as input to the DeleteWAFInput function.
 type DeleteWAFInput struct {
 	// Service is the ID of the service. Version is the specific configuration
 	// version. Both fields are required.
@@ -207,4 +206,170 @@ func (c *Client) DeleteWAF(i *DeleteWAFInput) error {
 	path := fmt.Sprintf("/service/%s/version/%d/wafs/%s", i.Service, i.Version, i.ID)
 	_, err := c.Delete(path, nil)
 	return err
+}
+
+// OWASP is the information about an OWASP object.
+type OWASP struct {
+	ID                            string `jsonapi:"primary,owasp"`
+	AllowedHTTPVersions           string `jsonapi:"attr,allowed_http_versions"`
+	AllowedMethods                string `jsonapi:"attr,allowed_methods"`
+	AllowedRequestContentType     string `jsonapi:"attr,allowed_request_content_type"`
+	ArgLength                     int    `jsonapi:"attr,arg_length"`
+	ArgNameLength                 int    `jsonapi:"attr,arg_name_length"`
+	CombinedFileSizes             int    `jsonapi:"attr,combined_file_sizes"`
+	CreatedAt                     string `jsonapi:"attr,created_at"`
+	CriticalAnomalyScore          int    `jsonapi:"attr,critical_anomaly_score"`
+	CRSValidateUTF8Encoding       bool   `jsonapi:"attr,crs_validate_utf8_encoding"`
+	ErrorAnomalyScore             int    `jsonapi:"attr,error_anomaly_score"`
+	HighRiskCountryCodes          string `jsonapi:"attr,high_risk_country_codes"`
+	HTTPViolationScoreThreshold   int    `jsonapi:"attr,http_violation_score_threshold"`
+	InboundAnomalyScoreThreshold  int    `jsonapi:"attr,inbound_anomaly_score_threshold"`
+	LFIScoreThreshold             int    `jsonapi:"attr,lfi_score_threshold"`
+	MaxFileSize                   int    `jsonapi:"attr,max_file_size"`
+	MaxNumArgs                    int    `jsonapi:"attr,max_num_args"`
+	NoticeAnomalyScore            int    `jsonapi:"attr,notice_anomaly_score"`
+	ParanoiaLevel                 int    `jsonapi:"attr,paranoia_level"`
+	PHPInjectionScoreThreshold    int    `jsonapi:"attr,php_injection_score_threshold"`
+	RCEScoreThreshold             int    `jsonapi:"attr,rce_score_threshold"`
+	RestrictedExtensions          string `jsonapi:"attr,restricted_extensions"`
+	RestrictedHeaders             string `jsonapi:"attr,restricted_headers"`
+	RFIScoreThreshold             int    `jsonapi:"attr,rfi_score_threshold"`
+	SessionFixationScoreThreshold int    `jsonapi:"attr,session_fixation_score_threshold"`
+	SQLInjectionScoreThreshold    int    `jsonapi:"attr,sql_injection_score_threshold"`
+	TotalArgLength                int    `jsonapi:"attr,total_arg_length"`
+	UpdatedAt                     string `jsonapi:"attr,updated_at"`
+	WarningAnomalyScore           int    `jsonapi:"attr,warning_anomaly_score"`
+	XDDScoreThreshold             int    `jsonapi:"attr,xss_score_threshold"`
+}
+
+// GetOWASPInput is used as input to the GetOWASP function.
+type GetOWASPInput struct {
+	// Service is the ID of the service. WafID is the ID of the firewall.
+	// Both fields are required.
+	Service string
+	ID      string
+}
+
+// GetOWASP gets OWASP settings for a service firewall object.
+func (c *Client) GetOWASP(i *GetOWASPInput) (*OWASP, error) {
+	if i.Service == "" {
+		return nil, ErrMissingService
+	}
+
+	if i.ID == "" {
+		return nil, ErrMissingWAFID
+	}
+
+	path := fmt.Sprintf("/service/%s/wafs/%s/owasp", i.Service, i.ID)
+
+	resp, err := c.Get(path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var owasp OWASP
+	if err := jsonapi.UnmarshalPayload(resp.Body, &owasp); err != nil {
+		return nil, err
+	}
+	return &owasp, nil
+}
+
+// CreateOWASPInput is used as input to the CreateOWASP function.
+type CreateOWASPInput struct {
+	// Service is the ID of the service. WafID is the ID of the firewall.
+	// Both fields are required.
+	Service string
+	ID      string `jsonapi:"primary,owasp"`
+
+	Type string `jsonapi:"attr,type`
+}
+
+// CreateOWASP creates an OWASP settings object for a service firewall object.
+func (c *Client) CreateOWASP(i *CreateOWASPInput) (*OWASP, error) {
+	if i.Service == "" {
+		return nil, ErrMissingService
+	}
+
+	if i.ID == "" {
+		return nil, ErrMissingWAFID
+	}
+
+	path := fmt.Sprintf("/service/%s/wafs/%s/owasp", i.Service, i.ID)
+	resp, err := c.PostJSONAPI(path, i, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var owasp OWASP
+	if err := jsonapi.UnmarshalPayload(resp.Body, &owasp); err != nil {
+		return nil, err
+	}
+	return &owasp, nil
+}
+
+// CreateOWASPInput is used as input to the CreateOWASP function.
+type UpdateOWASPInput struct {
+	// Service is the ID of the service. WafID is the ID of the firewall.
+	// Both fields are required.
+	Service string
+	ID      string
+	OWASPID string `jsonapi:"primary,owasp,omitempty"`
+
+	Type                          string `jsonapi:"attr,type`
+	AllowedHTTPVersions           string `jsonapi:"attr,allowed_http_versions,omitempty"`
+	AllowedMethods                string `jsonapi:"attr,allowed_methods,omitempty"`
+	AllowedRequestContentType     string `jsonapi:"attr,allowed_request_content_type,omitempty"`
+	ArgLength                     int    `jsonapi:"attr,arg_length,omitempty"`
+	ArgNameLength                 int    `jsonapi:"attr,arg_name_length,omitempty"`
+	CombinedFileSizes             int    `jsonapi:"attr,combined_file_sizes,omitempty"`
+	CreatedAt                     string `jsonapi:"attr,created_at,omitempty"`
+	CriticalAnomalyScore          int    `jsonapi:"attr,critical_anomaly_score,omitempty"`
+	CRSValidateUTF8Encoding       bool   `jsonapi:"attr,crs_validate_utf8_encoding,omitempty"`
+	ErrorAnomalyScore             int    `jsonapi:"attr,error_anomaly_score,omitempty"`
+	HighRiskCountryCodes          string `jsonapi:"attr,high_risk_country_codes,omitempty"`
+	HTTPViolationScoreThreshold   int    `jsonapi:"attr,http_violation_score_threshold,omitempty"`
+	InboundAnomalyScoreThreshold  int    `jsonapi:"attr,inbound_anomaly_score_threshold,omitempty"`
+	LFIScoreThreshold             int    `jsonapi:"attr,lfi_score_threshold,omitempty"`
+	MaxFileSize                   int    `jsonapi:"attr,max_file_size,omitempty"`
+	MaxNumArgs                    int    `jsonapi:"attr,max_num_args,omitempty"`
+	NoticeAnomalyScore            int    `jsonapi:"attr,notice_anomaly_score,omitempty"`
+	ParanoiaLevel                 int    `jsonapi:"attr,paranoia_level,omitempty"`
+	PHPInjectionScoreThreshold    int    `jsonapi:"attr,php_injection_score_threshold,omitempty"`
+	RCEScoreThreshold             int    `jsonapi:"attr,rce_score_threshold,omitempty"`
+	RestrictedExtensions          string `jsonapi:"attr,restricted_extensions,omitempty"`
+	RestrictedHeaders             string `jsonapi:"attr,restricted_headers,omitempty"`
+	RFIScoreThreshold             int    `jsonapi:"attr,rfi_score_threshold,omitempty"`
+	SessionFixationScoreThreshold int    `jsonapi:"attr,session_fixation_score_threshold,omitempty"`
+	SQLInjectionScoreThreshold    int    `jsonapi:"attr,sql_injection_score_threshold,omitempty"`
+	TotalArgLength                int    `jsonapi:"attr,total_arg_length,omitempty"`
+	UpdatedAt                     string `jsonapi:"attr,updated_at,omitempty"`
+	WarningAnomalyScore           int    `jsonapi:"attr,warning_anomaly_score,omitempty"`
+	XDDScoreThreshold             int    `jsonapi:"attr,xss_score_threshold,omitempty"`
+}
+
+// CreateOWASP creates an OWASP settings object for a service firewall object.
+func (c *Client) UpdateOWASP(i *UpdateOWASPInput) (*OWASP, error) {
+	if i.Service == "" {
+		return nil, ErrMissingService
+	}
+
+	if i.ID == "" {
+		return nil, ErrMissingWAFID
+	}
+
+	if i.OWASPID == "" {
+		return nil, ErrMissingOWASPID
+	}
+
+	path := fmt.Sprintf("/service/%s/wafs/%s/owasp", i.Service, i.ID)
+	resp, err := c.PatchJSONAPI(path, i, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var owasp OWASP
+	if err := jsonapi.UnmarshalPayload(resp.Body, &owasp); err != nil {
+		return nil, err
+	}
+	return &owasp, nil
 }
