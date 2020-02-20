721c986f4db458d339fc271e8e542ea28018e63b
Anders Chen <anders@wistia.com>
2020-02-20 06:34:28 -0500

    Add test for createPurgeRequest function

--- fastly/purge_test.go
+++ fastly/purge_test.go
@@ -24,3 +24,36 @@ func TestClient_Purge(t *testing.T) {
 		t.Error("bad id")
 	}
 }
+
+func TestClient_createPurgeRequest(t *testing.T) {
+	t.Parallel()
+
+	c := DefaultClient()
+	purgeUrl := "http://fastly.com/robots.txt"
+	req, err := c.createPurgeRequest(purgeUrl)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if req.Method != "POST" {
+		t.Error("method should be POST for non-HTTPS purges")
+	}
+
+	if req.URL.Path != "/purge/"+purgeUrl {
+		t.Errorf("url path for POST purges should be prefixed with purge/(url); got %s", req.URL.Path)
+	}
+
+	purgeUrl = "https://fastly.com/robots.txt"
+	req, err = c.createPurgeRequest(purgeUrl)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if req.Method != "PURGE" {
+		t.Error("method should be PURGE for HTTPS purges")
+	}
+
+	if req.URL.String() != purgeUrl {
+		t.Errorf("url path for PURGE purges should be the URL; got %s", req.URL.String())
+	}
+}
