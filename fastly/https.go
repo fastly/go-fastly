9e0b97fe291043c0cd848f25916cd5087af898d0
Ezekiel Templin <zeke@templ.in>
2020-04-20 14:03:34 -0700

    Add support for HTTPS logging endpoints
    HTTPS log endpoints are currently in Limited Availability. See the docs
    for more information: https://docs.fastly.com/en/guides/log-streaming-https

---
+++ fastly/https.go
@@ -0,0 +1,261 @@
+package fastly
+
+import (
+	"fmt"
+	"net/url"
+	"sort"
+	"time"
+)
+
+// HTTPS represents an HTTPS Logging response from the Fastly API.
+type HTTPS struct {
+	ServiceID string `mapstructure:"service_id"`
+	Version   int    `mapstructure:"version"`
+
+	Name              string     `mapstructure:"name"`
+	ResponseCondition string     `mapstructure:"response_condition"`
+	Format            string     `mapstructure:"format"`
+	URL               string     `mapstructure:"url"`
+	RequestMaxEntries uint       `mapstructure:"request_max_entries"`
+	RequestMaxBytes   uint       `mapstructure:"request_max_bytes"`
+	ContentType       string     `mapstructure:"content_type"`
+	HeaderName        string     `mapstructure:"header_name"`
+	HeaderValue       string     `mapstructure:"header_value"`
+	Method            string     `mapstructure:"method"`
+	JSONFormat        string     `mapstructure:"json_format"`
+	Placement         string     `mapstructure:"placement"`
+	TLSCACert         string     `mapstructure:"tls_ca_cert"`
+	TLSClientCert     string     `mapstructure:"tls_client_cert"`
+	TLSClientKey      string     `mapstructure:"tls_client_key"`
+	TLSHostname       string     `mapstructure:"tls_hostname"`
+	MessageType       string     `mapstructure:"message_type"`
+	FormatVersion     uint       `mapstructure:"format_version"`
+	CreatedAt         *time.Time `mapstructure:"created_at"`
+	UpdatedAt         *time.Time `mapstructure:"updated_at"`
+	DeletedAt         *time.Time `mapstructure:"deleted_at"`
+}
+
+// httpsByName is a sortable list of HTTPS logs.
+type httpsByName []*HTTPS
+
+// Len, Swap, and Less implement the sortable interface.
+func (s httpsByName) Len() int      { return len(s) }
+func (s httpsByName) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s httpsByName) Less(i, j int) bool {
+	return s[i].Name < s[j].Name
+}
+
+// ListHTTPSInput is used as input to the ListHTTPS function.
+type ListHTTPSInput struct {
+	// Service is the ID of the service (required).
+	Service string
+
+	// Version is the specific configuration version (required).
+	Version int
+}
+
+// ListHTTPS returns the list of HTTPS logs for the configuration version.
+func (c *Client) ListHTTPS(i *ListHTTPSInput) ([]*HTTPS, error) {
+	if i.Service == "" {
+		return nil, ErrMissingService
+	}
+
+	if i.Version == 0 {
+		return nil, ErrMissingVersion
+	}
+
+	path := fmt.Sprintf("/service/%s/version/%d/logging/https", i.Service, i.Version)
+	resp, err := c.Get(path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var https []*HTTPS
+	if err := decodeJSON(&https, resp.Body); err != nil {
+		return nil, err
+	}
+	sort.Stable(httpsByName(https))
+	return https, nil
+}
+
+// CreateHTTPSInput is used as input to the CreateHTTPS function.
+type CreateHTTPSInput struct {
+	// Service is the ID of the service. Version is the specific configuration
+	// version. Both fields are required.
+	Service string
+	Version int
+
+	Name              string `form:"name,omitempty"`
+	ResponseCondition string `form:"response_condition,omitempty"`
+	Format            string `form:"format,omitempty"`
+	URL               string `form:"url,omitempty"`
+	RequestMaxEntries uint   `form:"request_max_entries,omitempty"`
+	RequestMaxBytes   uint   `form:"request_max_bytes,omitempty"`
+	ContentType       string `form:"content_type,omitempty"`
+	HeaderName        string `form:"header_name,omitempty"`
+	HeaderValue       string `form:"header_value,omitempty"`
+	Method            string `form:"method,omitempty"`
+	JSONFormat        string `form:"json_format,omitempty"`
+	Placement         string `form:"placement,omitempty"`
+	TLSCACert         string `form:"tls_ca_cert,omitempty"`
+	TLSClientCert     string `form:"tls_client_cert,omitempty"`
+	TLSClientKey      string `form:"tls_client_key,omitempty"`
+	TLSHostname       string `form:"tls_hostname,omitempty"`
+	MessageType       string `form:"message_type,omitempty"`
+	FormatVersion     uint   `form:"format_version,omitempty"`
+}
+
+// CreateHTTPS creates a new Fastly HTTPS logging endpoint.
+func (c *Client) CreateHTTPS(i *CreateHTTPSInput) (*HTTPS, error) {
+	if i.Service == "" {
+		return nil, ErrMissingService
+	}
+
+	if i.Version == 0 {
+		return nil, ErrMissingVersion
+	}
+
+	path := fmt.Sprintf("/service/%s/version/%d/logging/https", i.Service, i.Version)
+	resp, err := c.PostForm(path, i, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var https *HTTPS
+	if err := decodeJSON(&https, resp.Body); err != nil {
+		return nil, err
+	}
+	return https, nil
+}
+
+// GetHTTPSInput is used as input to the GetHTTPS function.
+type GetHTTPSInput struct {
+	// Service is the ID of the service. Version is the specific configuration
+	// version. Both fields are required.
+	Service string
+	Version int
+
+	// Name is the name of the HTTPS endpoint to fetch.
+	Name string
+}
+
+func (c *Client) GetHTTPS(i *GetHTTPSInput) (*HTTPS, error) {
+	if i.Service == "" {
+		return nil, ErrMissingService
+	}
+
+	if i.Version == 0 {
+		return nil, ErrMissingVersion
+	}
+
+	if i.Name == "" {
+		return nil, ErrMissingName
+	}
+
+	path := fmt.Sprintf("/service/%s/version/%d/logging/https/%s", i.Service, i.Version, url.PathEscape(i.Name))
+	resp, err := c.Get(path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var h *HTTPS
+	if err := decodeJSON(&h, resp.Body); err != nil {
+		return nil, err
+	}
+
+	return h, nil
+}
+
+type UpdateHTTPSInput struct {
+	// Service is the ID of the service. Version is the specific configuration
+	// version. Both fields are required.
+	Service string
+	Version int
+
+	// Name is the name of the HTTPS endpoint to fetch.
+	Name string
+
+	NewName           string `form:"name,omitempty"`
+	ResponseCondition string `form:"response_condition,omitempty"`
+	Format            string `form:"format,omitempty"`
+	URL               string `form:"url,omitempty"`
+	RequestMaxEntries uint   `form:"request_max_entries,omitempty"`
+	RequestMaxBytes   uint   `form:"request_max_bytes,omitempty"`
+	ContentType       string `form:"content_type,omitempty"`
+	HeaderName        string `form:"header_name,omitempty"`
+	HeaderValue       string `form:"header_value,omitempty"`
+	Method            string `form:"method,omitempty"`
+	JSONFormat        string `form:"json_format,omitempty"`
+	Placement         string `form:"placement,omitempty"`
+	TLSCACert         string `form:"tls_ca_cert,omitempty"`
+	TLSClientCert     string `form:"tls_client_cert,omitempty"`
+	TLSClientKey      string `form:"tls_client_key,omitempty"`
+	TLSHostname       string `form:"tls_hostname,omitempty"`
+	MessageType       string `form:"message_type,omitempty"`
+	FormatVersion     uint   `form:"format_version,omitempty"`
+}
+
+func (c *Client) UpdateHTTPS(i *UpdateHTTPSInput) (*HTTPS, error) {
+	if i.Service == "" {
+		return nil, ErrMissingService
+	}
+
+	if i.Version == 0 {
+		return nil, ErrMissingVersion
+	}
+
+	if i.Name == "" {
+		return nil, ErrMissingName
+	}
+
+	path := fmt.Sprintf("/service/%s/version/%d/logging/https/%s", i.Service, i.Version, url.PathEscape(i.Name))
+	resp, err := c.PutForm(path, i, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var h *HTTPS
+	if err := decodeJSON(&h, resp.Body); err != nil {
+		return nil, err
+	}
+	return h, nil
+}
+
+type DeleteHTTPSInput struct {
+	// Service is the ID of the service. Version is the specific configuration
+	// version. Both fields are required.
+	Service string
+	Version int
+
+	// Name is the name of the HTTPS endpoint to fetch.
+	Name string
+}
+
+func (c *Client) DeleteHTTPS(i *DeleteHTTPSInput) error {
+	if i.Service == "" {
+		return ErrMissingService
+	}
+
+	if i.Version == 0 {
+		return ErrMissingVersion
+	}
+
+	if i.Name == "" {
+		return ErrMissingName
+	}
+
+	path := fmt.Sprintf("/service/%s/version/%d/logging/https/%s", i.Service, i.Version, url.PathEscape(i.Name))
+	resp, err := c.Delete(path, nil)
+	if err != nil {
+		return err
+	}
+
+	var r *statusResp
+	if err := decodeJSON(&r, resp.Body); err != nil {
+		return err
+	}
+	if !r.Ok() {
+		return fmt.Errorf("Not Ok")
+	}
+	return nil
+}
---
+++ fastly/https_test.go
@@ -0,0 +1,356 @@
+package fastly
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestClient_HTTPS(t *testing.T) {
+	t.Parallel()
+
+	var err error
+	var tv *Version
+	record(t, "https/version", func(c *Client) {
+		tv = testVersion(t, c)
+	})
+
+	caCert := strings.TrimSpace(`
+-----BEGIN CERTIFICATE-----
+MIICUTCCAfugAwIBAgIBADANBgkqhkiG9w0BAQQFADBXMQswCQYDVQQGEwJDTjEL
+MAkGA1UECBMCUE4xCzAJBgNVBAcTAkNOMQswCQYDVQQKEwJPTjELMAkGA1UECxMC
+VU4xFDASBgNVBAMTC0hlcm9uZyBZYW5nMB4XDTA1MDcxNTIxMTk0N1oXDTA1MDgx
+NDIxMTk0N1owVzELMAkGA1UEBhMCQ04xCzAJBgNVBAgTAlBOMQswCQYDVQQHEwJD
+TjELMAkGA1UEChMCT04xCzAJBgNVBAsTAlVOMRQwEgYDVQQDEwtIZXJvbmcgWWFu
+ZzBcMA0GCSqGSIb3DQEBAQUAA0sAMEgCQQCp5hnG7ogBhtlynpOS21cBewKE/B7j
+V14qeyslnr26xZUsSVko36ZnhiaO/zbMOoRcKK9vEcgMtcLFuQTWDl3RAgMBAAGj
+gbEwga4wHQYDVR0OBBYEFFXI70krXeQDxZgbaCQoR4jUDncEMH8GA1UdIwR4MHaA
+FFXI70krXeQDxZgbaCQoR4jUDncEoVukWTBXMQswCQYDVQQGEwJDTjELMAkGA1UE
+CBMCUE4xCzAJBgNVBAcTAkNOMQswCQYDVQQKEwJPTjELMAkGA1UECxMCVU4xFDAS
+BgNVBAMTC0hlcm9uZyBZYW5nggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEE
+BQADQQA/ugzBrjjK9jcWnDVfGHlk3icNRq0oV7Ri32z/+HQX67aRfgZu7KWdI+Ju
+Wm7DCfrPNGVwFWUQOmsPue9rZBgO
+-----END CERTIFICATE-----
+`)
+	clientCert := strings.TrimSpace(certificate())
+	clientKey := strings.TrimSpace(privateKey())
+
+	// Create
+	var h *HTTPS
+	record(t, "https/create", func(c *Client) {
+		h, err = c.CreateHTTPS(&CreateHTTPSInput{
+			Service:           testServiceID,
+			Version:           tv.Number,
+			Name:              "test-https",
+			Format:            "format",
+			URL:               "https://example.com/",
+			RequestMaxEntries: 1,
+			RequestMaxBytes:   1000,
+			ContentType:       "application/json",
+			HeaderName:        "X-Example-Header",
+			HeaderValue:       "ExampleValue",
+			Method:            "PUT",
+			JSONFormat:        "2",
+			Placement:         "waf_debug",
+			TLSCACert:         caCert,
+			TLSClientCert:     clientCert,
+			TLSClientKey:      clientKey,
+			TLSHostname:       "example.com",
+			MessageType:       "blank",
+			FormatVersion:     2,
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// ensure deleted
+	defer func() {
+		record(t, "https/cleanup", func(c *Client) {
+			c.DeleteHTTPS(&DeleteHTTPSInput{
+				Service: testServiceID,
+				Version: tv.Number,
+				Name:    "test-https",
+			})
+
+			// ensure that renamed endpoint created in Update test is deleted
+			c.DeleteHTTPS(&DeleteHTTPSInput{
+				Service: testServiceID,
+				Version: tv.Number,
+				Name:    "new-test-https",
+			})
+		})
+	}()
+
+	if h.Name != "test-https" {
+		t.Errorf("bad name: %q", h.Name)
+	}
+	if h.Format != "format" {
+		t.Errorf("bad format: %q", h.Format)
+	}
+	if h.URL != "https://example.com/" {
+		t.Errorf("bad url: %q", h.URL)
+	}
+	if h.RequestMaxEntries != 1 {
+		t.Errorf("bad request_max_entries: %q", h.RequestMaxEntries)
+	}
+	if h.RequestMaxBytes != 1000 {
+		t.Errorf("bad request_max_bytes: %q", h.RequestMaxBytes)
+	}
+	if h.ContentType != "application/json" {
+		t.Errorf("bad content_type: %q", h.ContentType)
+	}
+	if h.HeaderName != "X-Example-Header" {
+		t.Errorf("bad header_name: %q", h.HeaderName)
+	}
+	if h.HeaderValue != "ExampleValue" {
+		t.Errorf("bad header_value: %q", h.HeaderValue)
+	}
+	if h.Method != "PUT" {
+		t.Errorf("bad method: %q", h.Method)
+	}
+	if h.JSONFormat != "2" {
+		t.Errorf("bad json_format: %q", h.JSONFormat)
+	}
+	if h.Placement != "waf_debug" {
+		t.Errorf("bad placement: %q", h.Placement)
+	}
+	if h.TLSCACert != caCert {
+		t.Errorf("bad tls_ca_cert: %q", h.TLSCACert)
+	}
+	if h.TLSHostname != "example.com" {
+		t.Errorf("bad tls_hostname: %q", h.TLSHostname)
+	}
+	if h.TLSClientCert != clientCert {
+		t.Errorf("bad tls_client_cert: %q", h.TLSClientCert)
+	}
+	if h.TLSClientKey != clientKey {
+		t.Errorf("bad tls_client_key: %q", h.TLSClientKey)
+	}
+	if h.MessageType != "blank" {
+		t.Errorf("bad message_type: %s", h.MessageType)
+	}
+	if h.FormatVersion != 2 {
+		t.Errorf("bad format_version: %d", h.FormatVersion)
+	}
+
+	// List
+	var hs []*HTTPS
+	record(t, "https/list", func(c *Client) {
+		hs, err = c.ListHTTPS(&ListHTTPSInput{
+			Service: testServiceID,
+			Version: tv.Number,
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(hs) < 1 {
+		t.Errorf("bad https: %v", hs)
+	}
+
+	// Get
+	var nh *HTTPS
+	record(t, "https/get", func(c *Client) {
+		nh, err = c.GetHTTPS(&GetHTTPSInput{
+			Service: testServiceID,
+			Version: tv.Number,
+			Name:    "test-https",
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if h.Name != nh.Name {
+		t.Errorf("bad name: %q", h.Name)
+	}
+	if h.Format != nh.Format {
+		t.Errorf("bad format: %q", h.Format)
+	}
+	if h.URL != nh.URL {
+		t.Errorf("bad url: %q", h.URL)
+	}
+	if h.RequestMaxEntries != nh.RequestMaxEntries {
+		t.Errorf("bad request_max_entries: %q", h.RequestMaxEntries)
+	}
+	if h.RequestMaxBytes != nh.RequestMaxBytes {
+		t.Errorf("bad request_max_bytes: %q", h.RequestMaxBytes)
+	}
+	if h.ContentType != nh.ContentType {
+		t.Errorf("bad content_type: %q", h.ContentType)
+	}
+	if h.HeaderName != nh.HeaderName {
+		t.Errorf("bad header_name: %q", h.HeaderName)
+	}
+	if h.HeaderValue != nh.HeaderValue {
+		t.Errorf("bad header_value: %q", h.HeaderValue)
+	}
+	if h.Method != nh.Method {
+		t.Errorf("bad method: %q", h.Method)
+	}
+	if h.JSONFormat != nh.JSONFormat {
+		t.Errorf("bad json_format: %q", h.JSONFormat)
+	}
+	if h.Placement != nh.Placement {
+		t.Errorf("bad placement: %q", h.Placement)
+	}
+	if h.TLSCACert != nh.TLSCACert {
+		t.Errorf("bad tls_ca_cert: %q", h.TLSCACert)
+	}
+	if h.TLSHostname != nh.TLSHostname {
+		t.Errorf("bad tls_hostname: %q", h.TLSHostname)
+	}
+	if h.TLSClientCert != nh.TLSClientCert {
+		t.Errorf("bad tls_client_cert: %q", h.TLSClientCert)
+	}
+	if h.TLSClientKey != nh.TLSClientKey {
+		t.Errorf("bad tls_client_key: %q", h.TLSClientKey)
+	}
+	if h.MessageType != nh.MessageType {
+		t.Errorf("bad message_type: %s", h.MessageType)
+	}
+	if h.FormatVersion != nh.FormatVersion {
+		t.Errorf("bad format_version: %d", h.FormatVersion)
+	}
+
+	// Update
+	var uh *HTTPS
+	record(t, "https/update", func(c *Client) {
+		uh, err = c.UpdateHTTPS(&UpdateHTTPSInput{
+			Service: testServiceID,
+			Version: tv.Number,
+			Name:    "test-https",
+			NewName: "new-test-https",
+			Method:  "POST",
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if uh.Name != "new-test-https" {
+		t.Errorf("bad name: %q", uh.Name)
+	}
+	if uh.Method != "POST" {
+		t.Errorf("bad method: %q", uh.Method)
+	}
+
+	// Delete
+	record(t, "https/delete", func(c *Client) {
+		err = c.DeleteHTTPS(&DeleteHTTPSInput{
+			Service: testServiceID,
+			Version: tv.Number,
+			Name:    "new-test-https",
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClient_ListHTTPS_validation(t *testing.T) {
+	var err error
+	_, err = testClient.ListHTTPS(&ListHTTPSInput{
+		Service: "",
+	})
+	if err != ErrMissingService {
+		t.Errorf("bad error: %s", err)
+	}
+}
+
+func TestClient_CreateHTTPS_validation(t *testing.T) {
+	var err error
+	_, err = testClient.CreateHTTPS(&CreateHTTPSInput{
+		Service: "",
+	})
+	if err != ErrMissingService {
+		t.Errorf("bad error: %s", err)
+	}
+
+	_, err = testClient.CreateHTTPS(&CreateHTTPSInput{
+		Service: "foo",
+		Version: 0,
+	})
+	if err != ErrMissingVersion {
+		t.Errorf("bad error: %s", err)
+	}
+}
+
+func TestClient_GetHTTPS_validation(t *testing.T) {
+	var err error
+	_, err = testClient.GetHTTPS(&GetHTTPSInput{
+		Service: "",
+	})
+	if err != ErrMissingService {
+		t.Errorf("bad error: %s", err)
+	}
+
+	_, err = testClient.GetHTTPS(&GetHTTPSInput{
+		Service: "foo",
+		Version: 0,
+	})
+	if err != ErrMissingVersion {
+		t.Errorf("bad error: %s", err)
+	}
+
+	_, err = testClient.GetHTTPS(&GetHTTPSInput{
+		Service: "foo",
+		Version: 1,
+		Name:    "",
+	})
+	if err != ErrMissingName {
+		t.Errorf("bad error: %s", err)
+	}
+}
+
+func TestClient_UpdateHTTPS_validation(t *testing.T) {
+	var err error
+	_, err = testClient.UpdateHTTPS(&UpdateHTTPSInput{
+		Service: "",
+	})
+	if err != ErrMissingService {
+		t.Errorf("bad error: %s", err)
+	}
+
+	_, err = testClient.UpdateHTTPS(&UpdateHTTPSInput{
+		Service: "foo",
+		Version: 0,
+	})
+	if err != ErrMissingVersion {
+		t.Errorf("bad error: %s", err)
+	}
+
+	_, err = testClient.UpdateHTTPS(&UpdateHTTPSInput{
+		Service: "foo",
+		Version: 1,
+		Name:    "",
+	})
+	if err != ErrMissingName {
+		t.Errorf("bad error: %s", err)
+	}
+}
+
+func TestClient_DeleteHTTPS_validation(t *testing.T) {
+	var err error
+	err = testClient.DeleteHTTPS(&DeleteHTTPSInput{
+		Service: "",
+	})
+	if err != ErrMissingService {
+		t.Errorf("bad error: %s", err)
+	}
+
+	err = testClient.DeleteHTTPS(&DeleteHTTPSInput{
+		Service: "foo",
+		Version: 0,
+	})
+	if err != ErrMissingVersion {
+		t.Errorf("bad error: %s", err)
+	}
+
+	err = testClient.DeleteHTTPS(&DeleteHTTPSInput{
+		Service: "foo",
+		Version: 1,
+		Name:    "",
+	})
+	if err != ErrMissingName {
+		t.Errorf("bad error: %s", err)
+	}
+}
