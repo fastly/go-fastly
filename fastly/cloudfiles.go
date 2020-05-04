613db71c5797ff40ad828be90c79738a4ace29c6
Ezekiel Templin <zeke@templ.in>
2020-05-04 16:09:54 -0700

    Add Rackspace Cloud Files endpoint support

---
+++ fastly/cloudfiles.go
@@ -0,0 +1,255 @@
+package fastly
+
+import (
+	"fmt"
+	"net/url"
+	"sort"
+	"time"
+)
+
+// Cloudfiles represents a Cloudfiles response from the Fastly API.
+type Cloudfiles struct {
+	ServiceID string `mapstructure:"service_id"`
+	Version   int    `mapstructure:"version"`
+
+	Name              string     `mapstructure:"name"`
+	User              string     `mapstructure:"user"`
+	AccessKey         string     `mapstructure:"access_key"`
+	BucketName        string     `mapstructure:"bucket_name"`
+	Path              string     `mapstructure:"path"`
+	Region            string     `mapstructure:"region"`
+	Placement         string     `mapstructure:"placement"`
+	Period            uint       `mapstructure:"period"`
+	GzipLevel         uint       `mapstructure:"gzip_level"`
+	Format            string     `mapstructure:"format"`
+	FormatVersion     uint       `mapstructure:"format_version"`
+	ResponseCondition string     `mapstructure:"response_condition"`
+	MessageType       string     `mapstructure:"message_type"`
+	TimestampFormat   string     `mapstructure:"timestamp_format"`
+	PublicKey         string     `mapstructure:"public_key"`
+	CreatedAt         *time.Time `mapstructure:"created_at"`
+	UpdatedAt         *time.Time `mapstructure:"updated_at"`
+	DeletedAt         *time.Time `mapstructure:"deleted_at"`
+}
+
+// cloudfilesByName is a sortable list of Cloudfiles.
+type cloudfilesByName []*Cloudfiles
+
+// Len, Swap, and Less implement the sortable interface.
+func (c cloudfilesByName) Len() int      { return len(c) }
+func (c cloudfilesByName) Swap(i, j int) { c[i], c[j] = c[j], c[i] }
+func (c cloudfilesByName) Less(i, j int) bool {
+	return c[i].Name < c[j].Name
+}
+
+// ListCloudfilesInput is used as input to the ListCloudfiles function.
+type ListCloudfilesInput struct {
+	// Service is the ID of the service (required).
+	Service string
+
+	// Version is the specific configuration version (required).
+	Version int
+}
+
+// ListCloudfiles returns the list of Cloudfiles for the configuration version.
+func (c *Client) ListCloudfiles(i *ListCloudfilesInput) ([]*Cloudfiles, error) {
+	if i.Service == "" {
+		return nil, ErrMissingService
+	}
+
+	if i.Version == 0 {
+		return nil, ErrMissingVersion
+	}
+
+	path := fmt.Sprintf("/service/%s/version/%d/logging/cloudfiles", i.Service, i.Version)
+	resp, err := c.Get(path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var cloudfiles []*Cloudfiles
+	if err := decodeJSON(&cloudfiles, resp.Body); err != nil {
+		return nil, err
+	}
+	sort.Stable(cloudfilesByName(cloudfiles))
+	return cloudfiles, nil
+}
+
+// CreateCloudfilesInput is used as input to the CreateCloudfiles function.
+type CreateCloudfilesInput struct {
+	// Service is the ID of the service. Version is the specific configuration
+	// version. Both fields are required.
+	Service string
+	Version int
+
+	Name              string `form:"name,omitempty"`
+	User              string `form:"user,omitempty"`
+	AccessKey         string `form:"access_key,omitempty"`
+	BucketName        string `form:"bucket_name,omitempty"`
+	Path              string `form:"path,omitempty"`
+	Region            string `form:"region,omitempty"`
+	Placement         string `form:"placement,omitempty"`
+	Period            uint   `form:"period,omitempty"`
+	GzipLevel         uint   `form:"gzip_level,omitempty"`
+	Format            string `form:"format,omitempty"`
+	FormatVersion     uint   `form:"format_version,omitempty"`
+	ResponseCondition string `form:"response_condition,omitempty"`
+	MessageType       string `form:"message_type,omitempty"`
+	TimestampFormat   string `form:"timestamp_format,omitempty"`
+	PublicKey         string `form:"public_key,omitempty"`
+}
+
+// CreateCloudfiles creates a new Fastly Cloudfiles.
+func (c *Client) CreateCloudfiles(i *CreateCloudfilesInput) (*Cloudfiles, error) {
+	if i.Service == "" {
+		return nil, ErrMissingService
+	}
+
+	if i.Version == 0 {
+		return nil, ErrMissingVersion
+	}
+
+	path := fmt.Sprintf("/service/%s/version/%d/logging/cloudfiles", i.Service, i.Version)
+	resp, err := c.PostForm(path, i, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var cloudfiles *Cloudfiles
+	if err := decodeJSON(&cloudfiles, resp.Body); err != nil {
+		return nil, err
+	}
+	return cloudfiles, nil
+}
+
+// GetCloudfilesInput is used as input to the GetCloudfiles function.
+type GetCloudfilesInput struct {
+	// Service is the ID of the service. Version is the specific configuration
+	// version. Both fields are required.
+	Service string
+	Version int
+
+	// Name is the name of the Cloudfiles to fetch.
+	Name string
+}
+
+// GetCloudfiles gets the Cloudfiles configuration with the given parameters.
+func (c *Client) GetCloudfiles(i *GetCloudfilesInput) (*Cloudfiles, error) {
+	if i.Service == "" {
+		return nil, ErrMissingService
+	}
+
+	if i.Version == 0 {
+		return nil, ErrMissingVersion
+	}
+
+	if i.Name == "" {
+		return nil, ErrMissingName
+	}
+
+	path := fmt.Sprintf("/service/%s/version/%d/logging/cloudfiles/%s", i.Service, i.Version, url.PathEscape(i.Name))
+	resp, err := c.Get(path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var cloudfiles *Cloudfiles
+	if err := decodeJSON(&cloudfiles, resp.Body); err != nil {
+		return nil, err
+	}
+	return cloudfiles, nil
+}
+
+// UpdateCloudfilesInput is used as input to the UpdateCloudfiles function.
+type UpdateCloudfilesInput struct {
+	// Service is the ID of the service. Version is the specific configuration
+	// version. Both fields are required.
+	Service string
+	Version int
+
+	// Name is the name of the Cloudfiles to update.
+	Name string
+
+	NewName           string `form:"name,omitempty"`
+	AccessKey         string `form:"access_key,omitempty"`
+	BucketName        string `form:"bucket_name,omitempty"`
+	Path              string `form:"path,omitempty"`
+	Region            string `form:"region,omitempty"`
+	Placement         string `form:"placement,omitempty"`
+	Period            uint   `form:"period,omitempty"`
+	GzipLevel         uint   `form:"gzip_level,omitempty"`
+	Format            string `form:"format,omitempty"`
+	FormatVersion     uint   `form:"format_version,omitempty"`
+	ResponseCondition string `form:"response_condition,omitempty"`
+	MessageType       string `form:"message_type,omitempty"`
+	TimestampFormat   string `form:"timestamp_format,omitempty"`
+	PublicKey         string `form:"public_key,omitempty"`
+}
+
+// UpdateCloudfiles updates a specific Cloudfiles.
+func (c *Client) UpdateCloudfiles(i *UpdateCloudfilesInput) (*Cloudfiles, error) {
+	if i.Service == "" {
+		return nil, ErrMissingService
+	}
+
+	if i.Version == 0 {
+		return nil, ErrMissingVersion
+	}
+
+	if i.Name == "" {
+		return nil, ErrMissingName
+	}
+
+	path := fmt.Sprintf("/service/%s/version/%d/logging/cloudfiles/%s", i.Service, i.Version, url.PathEscape(i.Name))
+	resp, err := c.PutForm(path, i, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var cloudfiles *Cloudfiles
+	if err := decodeJSON(&cloudfiles, resp.Body); err != nil {
+		return nil, err
+	}
+	return cloudfiles, nil
+}
+
+// DeleteCloudfilesInput is the input parameter to DeleteCloudfiles.
+type DeleteCloudfilesInput struct {
+	// Service is the ID of the service. Version is the specific configuration
+	// version. Both fields are required.
+	Service string
+	Version int
+
+	// Name is the name of the Cloudfiles to delete (required).
+	Name string
+}
+
+// DeleteCloudfiles deletes the given Cloudfiles version.
+func (c *Client) DeleteCloudfiles(i *DeleteCloudfilesInput) error {
+	if i.Service == "" {
+		return ErrMissingService
+	}
+
+	if i.Version == 0 {
+		return ErrMissingVersion
+	}
+
+	if i.Name == "" {
+		return ErrMissingName
+	}
+
+	path := fmt.Sprintf("/service/%s/version/%d/logging/cloudfiles/%s", i.Service, i.Version, url.PathEscape(i.Name))
+	resp, err := c.Delete(path, nil)
+	if err != nil {
+		return err
+	}
+
+	var r *statusResp
+	if err := decodeJSON(&r, resp.Body); err != nil {
+		return err
+	}
+	if !r.Ok() {
+		return fmt.Errorf("Not Ok")
+	}
+	return nil
+}
---
+++ fastly/cloudfiles_test.go
@@ -0,0 +1,315 @@
+package fastly
+
+import "testing"
+
+func TestClient_Cloudfiles(t *testing.T) {
+	t.Parallel()
+
+	var err error
+	var tv *Version
+	record(t, "cloudfiles/version", func(c *Client) {
+		tv = testVersion(t, c)
+	})
+
+	// Create
+	var cloudfiles *Cloudfiles
+	record(t, "cloudfiles/create", func(c *Client) {
+		cloudfiles, err = c.CreateCloudfiles(&CreateCloudfilesInput{
+			Service:         testServiceID,
+			Version:         tv.Number,
+			Name:            "test-cloudfiles",
+			User:            "user",
+			AccessKey:       "secret-key",
+			BucketName:      "bucket-name",
+			Path:            "/path",
+			Region:          "DFW",
+			Period:          12,
+			GzipLevel:       9,
+			Format:          "format",
+			FormatVersion:   2,
+			TimestampFormat: "%Y",
+			MessageType:     "classic",
+			Placement:       "waf_debug",
+			PublicKey:       pgpPublicKey(),
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Ensure deleted
+	defer func() {
+		record(t, "cloudfiles/cleanup", func(c *Client) {
+			c.DeleteCloudfiles(&DeleteCloudfilesInput{
+				Service: testServiceID,
+				Version: tv.Number,
+				Name:    "test-cloudfiles",
+			})
+
+			c.DeleteCloudfiles(&DeleteCloudfilesInput{
+				Service: testServiceID,
+				Version: tv.Number,
+				Name:    "new-test-cloudfiles",
+			})
+		})
+	}()
+
+	if cloudfiles.Name != "test-cloudfiles" {
+		t.Errorf("bad name: %q", cloudfiles.Name)
+	}
+	if cloudfiles.User != "user" {
+		t.Errorf("bad user: %q", cloudfiles.User)
+	}
+	if cloudfiles.BucketName != "bucket-name" {
+		t.Errorf("bad bucket_name: %q", cloudfiles.BucketName)
+	}
+	if cloudfiles.AccessKey != "secret-key" {
+		t.Errorf("bad access_key: %q", cloudfiles.AccessKey)
+	}
+	if cloudfiles.Path != "/path" {
+		t.Errorf("bad path: %q", cloudfiles.Path)
+	}
+	if cloudfiles.Region != "DFW" {
+		t.Errorf("bad region: %q", cloudfiles.Region)
+	}
+	if cloudfiles.Period != 12 {
+		t.Errorf("bad period: %q", cloudfiles.Period)
+	}
+	if cloudfiles.GzipLevel != 9 {
+		t.Errorf("bad gzip_level: %q", cloudfiles.GzipLevel)
+	}
+	if cloudfiles.Format != "format" {
+		t.Errorf("bad format: %q", cloudfiles.Format)
+	}
+	if cloudfiles.FormatVersion != 2 {
+		t.Errorf("bad format_version: %q", cloudfiles.FormatVersion)
+	}
+	if cloudfiles.TimestampFormat != "%Y" {
+		t.Errorf("bad timestamp_format: %q", cloudfiles.TimestampFormat)
+	}
+	if cloudfiles.MessageType != "classic" {
+		t.Errorf("bad message_type: %q", cloudfiles.MessageType)
+	}
+	if cloudfiles.Placement != "waf_debug" {
+		t.Errorf("bad placement: %q", cloudfiles.Placement)
+	}
+	if cloudfiles.PublicKey != pgpPublicKey() {
+		t.Errorf("bad public_key: %q", cloudfiles.PublicKey)
+	}
+
+	// List
+	var lc []*Cloudfiles
+	record(t, "cloudfiles/list", func(c *Client) {
+		lc, err = c.ListCloudfiles(&ListCloudfilesInput{
+			Service: testServiceID,
+			Version: tv.Number,
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(lc) < 1 {
+		t.Errorf("bad cloudfiles: %v", lc)
+	}
+
+	// Get
+	var ncloudfiles *Cloudfiles
+	record(t, "cloudfiles/get", func(c *Client) {
+		ncloudfiles, err = c.GetCloudfiles(&GetCloudfilesInput{
+			Service: testServiceID,
+			Version: tv.Number,
+			Name:    "test-cloudfiles",
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if cloudfiles.Name != ncloudfiles.Name {
+		t.Errorf("bad name: %q", cloudfiles.Name)
+	}
+	if cloudfiles.User != ncloudfiles.User {
+		t.Errorf("bad user: %q", cloudfiles.User)
+	}
+	if cloudfiles.BucketName != ncloudfiles.BucketName {
+		t.Errorf("bad bucket_name: %q", cloudfiles.BucketName)
+	}
+	if cloudfiles.AccessKey != ncloudfiles.AccessKey {
+		t.Errorf("bad access_key: %q", cloudfiles.AccessKey)
+	}
+	if cloudfiles.Path != ncloudfiles.Path {
+		t.Errorf("bad path: %q", cloudfiles.Path)
+	}
+	if cloudfiles.Region != ncloudfiles.Region {
+		t.Errorf("bad region: %q", cloudfiles.Region)
+	}
+	if cloudfiles.Period != ncloudfiles.Period {
+		t.Errorf("bad period: %q", cloudfiles.Period)
+	}
+	if cloudfiles.GzipLevel != ncloudfiles.GzipLevel {
+		t.Errorf("bad gzip_level: %q", cloudfiles.GzipLevel)
+	}
+	if cloudfiles.Format != ncloudfiles.Format {
+		t.Errorf("bad format: %q", cloudfiles.Format)
+	}
+	if cloudfiles.FormatVersion != ncloudfiles.FormatVersion {
+		t.Errorf("bad format_version: %q", cloudfiles.FormatVersion)
+	}
+	if cloudfiles.TimestampFormat != ncloudfiles.TimestampFormat {
+		t.Errorf("bad timestamp_format: %q", cloudfiles.TimestampFormat)
+	}
+	if cloudfiles.MessageType != ncloudfiles.MessageType {
+		t.Errorf("bad message_type: %q", cloudfiles.MessageType)
+	}
+	if cloudfiles.Placement != ncloudfiles.Placement {
+		t.Errorf("bad placement: %q", cloudfiles.Placement)
+	}
+	if cloudfiles.PublicKey != ncloudfiles.PublicKey {
+		t.Errorf("bad public_key: %q", cloudfiles.PublicKey)
+	}
+
+	// Update
+	var ucloudfiles *Cloudfiles
+	record(t, "cloudfiles/update", func(c *Client) {
+		ucloudfiles, err = c.UpdateCloudfiles(&UpdateCloudfilesInput{
+			Service: testServiceID,
+			Version: tv.Number,
+			Name:    "test-cloudfiles",
+			NewName: "new-test-cloudfiles",
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ucloudfiles.Name != "new-test-cloudfiles" {
+		t.Errorf("bad name: %q", ucloudfiles.Name)
+	}
+
+	// Delete
+	record(t, "cloudfiles/delete", func(c *Client) {
+		err = c.DeleteCloudfiles(&DeleteCloudfilesInput{
+			Service: testServiceID,
+			Version: tv.Number,
+			Name:    "new-test-cloudfiles",
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClient_ListCloudfiles_validation(t *testing.T) {
+	var err error
+	_, err = testClient.ListCloudfiles(&ListCloudfilesInput{
+		Service: "",
+	})
+	if err != ErrMissingService {
+		t.Errorf("bad error: %s", err)
+	}
+
+	_, err = testClient.ListCloudfiles(&ListCloudfilesInput{
+		Service: "foo",
+		Version: 0,
+	})
+	if err != ErrMissingVersion {
+		t.Errorf("bad error: %s", err)
+	}
+}
+
+func TestClient_CreateCloudfiles_validation(t *testing.T) {
+	var err error
+	_, err = testClient.CreateCloudfiles(&CreateCloudfilesInput{
+		Service: "",
+	})
+	if err != ErrMissingService {
+		t.Errorf("bad error: %s", err)
+	}
+
+	_, err = testClient.CreateCloudfiles(&CreateCloudfilesInput{
+		Service: "foo",
+		Version: 0,
+	})
+	if err != ErrMissingVersion {
+		t.Errorf("bad error: %s", err)
+	}
+}
+
+func TestClient_GetCloudfiles_validation(t *testing.T) {
+	var err error
+	_, err = testClient.GetCloudfiles(&GetCloudfilesInput{
+		Service: "",
+	})
+	if err != ErrMissingService {
+		t.Errorf("bad error: %s", err)
+	}
+
+	_, err = testClient.GetCloudfiles(&GetCloudfilesInput{
+		Service: "foo",
+		Version: 0,
+	})
+	if err != ErrMissingVersion {
+		t.Errorf("bad error: %s", err)
+	}
+
+	_, err = testClient.GetCloudfiles(&GetCloudfilesInput{
+		Service: "foo",
+		Version: 1,
+		Name:    "",
+	})
+	if err != ErrMissingName {
+		t.Errorf("bad error: %s", err)
+	}
+}
+
+func TestClient_UpdateCloudfiles_validation(t *testing.T) {
+	var err error
+	_, err = testClient.UpdateCloudfiles(&UpdateCloudfilesInput{
+		Service: "",
+	})
+	if err != ErrMissingService {
+		t.Errorf("bad error: %s", err)
+	}
+
+	_, err = testClient.UpdateCloudfiles(&UpdateCloudfilesInput{
+		Service: "foo",
+		Version: 0,
+	})
+	if err != ErrMissingVersion {
+		t.Errorf("bad error: %s", err)
+	}
+
+	_, err = testClient.UpdateCloudfiles(&UpdateCloudfilesInput{
+		Service: "foo",
+		Version: 1,
+		Name:    "",
+	})
+	if err != ErrMissingName {
+		t.Errorf("bad error: %s", err)
+	}
+}
+
+func TestClient_DeleteCloudfiles_validation(t *testing.T) {
+	var err error
+	err = testClient.DeleteCloudfiles(&DeleteCloudfilesInput{
+		Service: "",
+	})
+	if err != ErrMissingService {
+		t.Errorf("bad error: %s", err)
+	}
+
+	err = testClient.DeleteCloudfiles(&DeleteCloudfilesInput{
+		Service: "foo",
+		Version: 0,
+	})
+	if err != ErrMissingVersion {
+		t.Errorf("bad error: %s", err)
+	}
+
+	err = testClient.DeleteCloudfiles(&DeleteCloudfilesInput{
+		Service: "foo",
+		Version: 1,
+		Name:    "",
+	})
+	if err != ErrMissingName {
+		t.Errorf("bad error: %s", err)
+	}
+}
