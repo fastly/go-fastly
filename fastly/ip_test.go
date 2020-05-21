f28b69f899d20c4a18140c5aa554df1ccefbdb7f
Billy Bautista <wbautista@expedia.com>
2020-05-21 15:48:28 -0500

    Exposed IPsV6 and AllIPs APIs. Added tests.

--- fastly/ip_test.go
+++ fastly/ip_test.go
@@ -17,3 +17,39 @@ func TestClient_IPs(t *testing.T) {
 		t.Fatal("missing ips")
 	}
 }
+
+func TestClient_IPsV6(t *testing.T) {
+	t.Parallel()
+
+	var err error
+	var ips IPAddrs
+	record(t, "ips/list", func(c *Client) {
+		ips, err = c.IPsV6()
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(ips) == 0 {
+		t.Fatal("missing ips")
+	}
+}
+
+func TestClient_AllIPs(t *testing.T) {
+	t.Parallel()
+
+	var err error
+	var v4 IPAddrs
+	var v6 IPAddrs
+	record(t, "ips/list", func(c *Client) {
+		v4, v6, err = c.AllIPs()
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(v4) == 0 {
+		t.Fatal("missing v4 ips")
+	}
+	if len(v6) == 0 {
+		t.Fatal("missing v6 ips")
+	}
+}
