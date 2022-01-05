f9aad9f27b7f27f3301efb4076ea641646d26bcd
Shohei Maeda <irt_m.jrsyo@ntworkers.com>
2022-01-05 20:17:43 +0900

    Add test

--- fastly/waf_rules_test.go
+++ fastly/waf_rules_test.go
@@ -74,6 +74,18 @@ func TestClient_WAF_Rules(t *testing.T) {
 	if fastlyRulesNumber-1 != len(rulesResp.Items) {
 		t.Errorf("expected %d rules: got %d", fastlyRulesNumber-1, len(rulesResp.Items))
 	}
+
+	record(t, fixtureBase+"/list_all_fastly_filter_by_rule_ids", func(c *Client) {
+		rulesResp, err = c.ListAllWAFRules(&ListAllWAFRulesInput{
+			FilterModSecIDs: []int{1010060, 1010070},
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(rulesResp.Items) != 2 {
+		t.Errorf("expected 2 rules: got %d", len(rulesResp.Items))
+	}
 }
 
 func TestClient_listWAFRules_formatFilters(t *testing.T) {
@@ -85,6 +97,7 @@ func TestClient_listWAFRules_formatFilters(t *testing.T) {
 			remote: &ListWAFRulesInput{
 				FilterTagNames:   []string{"tag1", "tag2"},
 				FilterPublishers: []string{"owasp", "trustwave"},
+				FilterModSecIDs:  []int{1010060, 1010070},
 				ExcludeMocSecIDs: []int{123456, 1234567},
 				PageSize:         2,
 				PageNumber:       2,
@@ -93,6 +106,7 @@ func TestClient_listWAFRules_formatFilters(t *testing.T) {
 			local: map[string]string{
 				"filter[waf_tags][name][in]":  "tag1,tag2",
 				"filter[publisher][in]":       "owasp,trustwave",
+				"filter[modsec_rule_id][in]":  "1010060,1010070",
 				"filter[modsec_rule_id][not]": "123456,1234567",
 				"page[size]":                  "2",
 				"page[number]":                "2",
