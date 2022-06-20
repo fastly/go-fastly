e07319cf0f457890f47ab8379defd06d903270db
Mark McDonnell <Integralist@users.noreply.github.com>
2022-06-20 14:39:35 +0100

    v6.4.0 (#350)

--- fastly/client.go
+++ fastly/client.go
@@ -48,7 +48,7 @@ const DefaultRealtimeStatsEndpoint = "https://rt.fastly.com"
 var ProjectURL = "github.com/fastly/go-fastly"
 
 // ProjectVersion is the version of this library.
-var ProjectVersion = "6.3.2"
+var ProjectVersion = "6.4.0"
 
 // UserAgent is the user agent for this particular client.
 var UserAgent = fmt.Sprintf("FastlyGo/%s (+%s; %s)",
