b9089f1801bfd48905a0158f6c4b604418e87ffb
Integralist <integralist@fastly.com>
2022-11-04 14:44:14 +0000

    Honeycomb

--- fastly/honeycomb.go
+++ fastly/honeycomb.go
@@ -13,7 +13,7 @@ type Honeycomb struct {
 	Dataset           string     `mapstructure:"dataset"`
 	DeletedAt         *time.Time `mapstructure:"deleted_at"`
 	Format            string     `mapstructure:"format"`
-	FormatVersion     uint       `mapstructure:"format_version"`
+	FormatVersion     int        `mapstructure:"format_version"`
 	Name              string     `mapstructure:"name"`
 	Placement         string     `mapstructure:"placement"`
 	ResponseCondition string     `mapstructure:"response_condition"`
@@ -54,7 +54,6 @@ func (c *Client) ListHoneycombs(i *ListHoneycombsInput) ([]*Honeycomb, error) {
 	if i.ServiceID == "" {
 		return nil, ErrMissingServiceID
 	}
-
 	if i.ServiceVersion == 0 {
 		return nil, ErrMissingServiceVersion
 	}
@@ -77,23 +76,23 @@ func (c *Client) ListHoneycombs(i *ListHoneycombsInput) ([]*Honeycomb, error) {
 // CreateHoneycombInput is used as input to the CreateHoneycomb function.
 type CreateHoneycombInput struct {
 	// Dataset is the Honeycomb Dataset you want to log to.
-	Dataset string `url:"dataset,omitempty"`
+	Dataset *string `url:"dataset,omitempty"`
 	// Format is a Fastly log format string. Must produce valid JSON that Honeycomb can ingest.
-	Format string `url:"format,omitempty"`
+	Format *string `url:"format,omitempty"`
 	// FormatVersion is the version of the custom logging format used for the configured endpoint.
-	FormatVersion uint `url:"format_version,omitempty"`
+	FormatVersion *int `url:"format_version,omitempty"`
 	// Name is the name for the real-time logging configuration.
-	Name string `url:"name,omitempty"`
+	Name *string `url:"name,omitempty"`
 	// Placement is where in the generated VCL the logging call should be placed.
-	Placement string `url:"placement,omitempty"`
+	Placement *string `url:"placement,omitempty"`
 	// ResponseCondition is the name of an existing condition in the configured endpoint, or leave blank to always execute.
-	ResponseCondition string `url:"response_condition,omitempty"`
+	ResponseCondition *string `url:"response_condition,omitempty"`
 	// ServiceID is the ID of the service (required).
-	ServiceID string
+	ServiceID string `url:"-"`
 	// ServiceVersion is the specific configuration version (required).
-	ServiceVersion int
+	ServiceVersion int `url:"-"`
 	// Token is the Write Key from the Account page of your Honeycomb account.
-	Token string `url:"token,omitempty"`
+	Token *string `url:"token,omitempty"`
 }
 
 // CreateHoneycomb creates a new resource.
@@ -103,9 +102,6 @@ func (c *Client) CreateHoneycomb(i *CreateHoneycombInput) (*Honeycomb, error) {
 	}
 	if i.ServiceVersion == 0 {
 		return nil, ErrMissingServiceVersion
-	}
-	if i.Token == "" {
-		return nil, ErrMissingToken
 	}
 
 	path := fmt.Sprintf("/service/%s/version/%d/logging/honeycomb", i.ServiceID, i.ServiceVersion)
@@ -124,7 +120,7 @@ func (c *Client) CreateHoneycomb(i *CreateHoneycombInput) (*Honeycomb, error) {
 
 // GetHoneycombInput is used as input to the GetHoneycomb function.
 type GetHoneycombInput struct {
-	// Name is the name for the real-time logging configuration.
+	// Name is the name for the real-time logging configuration (required).
 	Name string
 	// ServiceID is the ID of the service (required).
 	ServiceID string
@@ -134,14 +130,14 @@ type GetHoneycombInput struct {
 
 // GetHoneycomb retrieves the specified resource.
 func (c *Client) GetHoneycomb(i *GetHoneycombInput) (*Honeycomb, error) {
+	if i.Name == "" {
+		return nil, ErrMissingName
+	}
 	if i.ServiceID == "" {
 		return nil, ErrMissingServiceID
 	}
 	if i.ServiceVersion == 0 {
 		return nil, ErrMissingServiceVersion
-	}
-	if i.Name == "" {
-		return nil, ErrMissingName
 	}
 
 	path := fmt.Sprintf("/service/%s/version/%d/logging/honeycomb/%s", i.ServiceID, i.ServiceVersion, url.PathEscape(i.Name))
@@ -165,9 +161,9 @@ type UpdateHoneycombInput struct {
 	// Format is a Fastly log format string. Must produce valid JSON that Honeycomb can ingest.
 	Format *string `url:"format,omitempty"`
 	// FormatVersion is the version of the custom logging format used for the configured endpoint.
-	FormatVersion *uint `url:"format_version,omitempty"`
-	// Name is the name of the honeycomb to update.
-	Name string
+	FormatVersion *int `url:"format_version,omitempty"`
+	// Name is the name of the honeycomb to update (required).
+	Name string `url:"-"`
 	// NewName is the new name for the resource.
 	NewName *string `url:"name,omitempty"`
 	// Placement is where in the generated VCL the logging call should be placed.
@@ -175,26 +171,23 @@ type UpdateHoneycombInput struct {
 	// ResponseCondition is the name of an existing condition in the configured endpoint, or leave blank to always execute.
 	ResponseCondition *string `url:"response_condition,omitempty"`
 	// ServiceID is the ID of the service (required).
-	ServiceID string
+	ServiceID string `url:"-"`
 	// ServiceVersion is the specific configuration version (required).
-	ServiceVersion int
+	ServiceVersion int `url:"-"`
 	// Token is the Write Key from the Account page of your Honeycomb account.
 	Token *string `url:"token,omitempty"`
 }
 
 // UpdateHoneycomb updates the specified resource.
 func (c *Client) UpdateHoneycomb(i *UpdateHoneycombInput) (*Honeycomb, error) {
+	if i.Name == "" {
+		return nil, ErrMissingName
+	}
 	if i.ServiceID == "" {
 		return nil, ErrMissingServiceID
 	}
 	if i.ServiceVersion == 0 {
 		return nil, ErrMissingServiceVersion
-	}
-	if i.Name == "" {
-		return nil, ErrMissingName
-	}
-	if i.Token != nil && *i.Token == "" {
-		return nil, ErrTokenEmpty
 	}
 
 	path := fmt.Sprintf("/service/%s/version/%d/logging/honeycomb/%s", i.ServiceID, i.ServiceVersion, url.PathEscape(i.Name))
@@ -223,14 +216,14 @@ type DeleteHoneycombInput struct {
 
 // DeleteHoneycomb deletes the specified resource.
 func (c *Client) DeleteHoneycomb(i *DeleteHoneycombInput) error {
+	if i.Name == "" {
+		return ErrMissingName
+	}
 	if i.ServiceID == "" {
 		return ErrMissingServiceID
 	}
 	if i.ServiceVersion == 0 {
 		return ErrMissingServiceVersion
-	}
-	if i.Name == "" {
-		return ErrMissingName
 	}
 
 	path := fmt.Sprintf("/service/%s/version/%d/logging/honeycomb/%s", i.ServiceID, i.ServiceVersion, url.PathEscape(i.Name))
