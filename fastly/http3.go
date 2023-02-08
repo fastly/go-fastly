d1cc6c640da6c03e12896add9171acc17f69ab51
Mark McDonnell <Integralist@users.noreply.github.com>
2023-02-08 19:37:59 +0000

    feat(http3): implement HTTP3 API endpoints (#395)
    * feat(http3): implement HTTP3 API endpoints
    
    * refactor(http3): add json tags

---
+++ fastly/http3.go
@@ -0,0 +1,115 @@
+package fastly
+
+import (
+	"fmt"
+	"time"
+)
+
+// HTTP3 represents a response from the Fastly API.
+type HTTP3 struct {
+	CreatedAt       *time.Time `mapstructure:"created_at" json:"created_at"`
+	DeletedAt       *time.Time `mapstructure:"deleted_at" json:"deleted_at"`
+	FeatureRevision int        `mapstructure:"feature_revision" json:"feature_revision"`
+	ServiceID       string     `mapstructure:"service_id" json:"service_id"`
+	ServiceVersion  int        `mapstructure:"version" json:"version"`
+	UpdatedAt       *time.Time `mapstructure:"updated_at" json:"updated_at"`
+}
+
+// GetHTTP3Input is used as input to the GetHTTP3 function.
+type GetHTTP3Input struct {
+	// ServiceID is the ID of the service (required).
+	ServiceID string
+	// ServiceVersion is the specific configuration version (required).
+	ServiceVersion int
+}
+
+// GetHTTP3 retrieves the specified resource.
+func (c *Client) GetHTTP3(i *GetHTTP3Input) (*HTTP3, error) {
+	if i.ServiceID == "" {
+		return nil, ErrMissingServiceID
+	}
+	if i.ServiceVersion == 0 {
+		return nil, ErrMissingServiceVersion
+	}
+
+	path := fmt.Sprintf("/service/%s/version/%d/http3", i.ServiceID, i.ServiceVersion)
+	resp, err := c.Get(path, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var h *HTTP3
+	if err := decodeBodyMap(resp.Body, &h); err != nil {
+		return nil, err
+	}
+
+	return h, nil
+}
+
+// EnableHTTP3Input is used as input to the EnableHTTP3 function.
+type EnableHTTP3Input struct {
+	// FeatureRevision is the revision number of the HTTP/3 feature implementation.
+	FeatureRevision *int `url:"feature_revision,omitempty"`
+	// ServiceID is the ID of the service (required).
+	ServiceID string `url:"-"`
+	// ServiceVersion is the specific configuration version (required).
+	ServiceVersion int `url:"-"`
+}
+
+// EnableHTTP3 creates a new resource.
+func (c *Client) EnableHTTP3(i *EnableHTTP3Input) (*HTTP3, error) {
+	if i.ServiceID == "" {
+		return nil, ErrMissingServiceID
+	}
+	if i.ServiceVersion == 0 {
+		return nil, ErrMissingServiceVersion
+	}
+
+	path := fmt.Sprintf("/service/%s/version/%d/http3", i.ServiceID, i.ServiceVersion)
+	resp, err := c.PostForm(path, i, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var http3 *HTTP3
+	if err := decodeBodyMap(resp.Body, &http3); err != nil {
+		return nil, err
+	}
+	return http3, nil
+}
+
+// DisableHTTP3Input is the input parameter to the DisableHTTP3 function.
+type DisableHTTP3Input struct {
+	// ServiceID is the ID of the service (required).
+	ServiceID string
+	// ServiceVersion is the specific configuration version (required).
+	ServiceVersion int
+}
+
+// DisableHTTP3 deletes the specified resource.
+func (c *Client) DisableHTTP3(i *DisableHTTP3Input) error {
+	if i.ServiceID == "" {
+		return ErrMissingServiceID
+	}
+	if i.ServiceVersion == 0 {
+		return ErrMissingServiceVersion
+	}
+
+	path := fmt.Sprintf("/service/%s/version/%d/http3", i.ServiceID, i.ServiceVersion)
+	resp, err := c.Delete(path, nil)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	var r *statusResp
+	if err := decodeBodyMap(resp.Body, &r); err != nil {
+		return err
+	}
+	if !r.Ok() {
+		return ErrNotOK
+	}
+	return nil
+}
---
+++ fastly/http3_test.go
@@ -0,0 +1,127 @@
+package fastly
+
+import (
+	"testing"
+)
+
+func TestClient_HTTP3(t *testing.T) {
+	t.Parallel()
+
+	var err error
+	var tv *Version
+	record(t, "http3/version", func(c *Client) {
+		tv = testVersion(t, c)
+	})
+
+	// Enable HTTP3
+	var h *HTTP3
+	record(t, "http3/enable", func(c *Client) {
+		h, err = c.EnableHTTP3(&EnableHTTP3Input{
+			FeatureRevision: Int(1),
+			ServiceID:       testServiceID,
+			ServiceVersion:  tv.Number,
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if h.FeatureRevision != 1 {
+		t.Errorf("bad feature_revision: %d", h.FeatureRevision)
+	}
+
+	// Get HTTP3 status
+	var gh *HTTP3
+	record(t, "http3/get", func(c *Client) {
+		gh, err = c.GetHTTP3(&GetHTTP3Input{
+			ServiceID:      testServiceID,
+			ServiceVersion: tv.Number,
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if gh.FeatureRevision != 1 {
+		t.Errorf("bad feature_revision: %d", gh.FeatureRevision)
+	}
+
+	// Disable HTTP3
+	record(t, "http3/disable", func(c *Client) {
+		err = c.DisableHTTP3(&DisableHTTP3Input{
+			ServiceID:      testServiceID,
+			ServiceVersion: tv.Number,
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Get HTTP3 status again to check disabled
+	record(t, "http3/get-disabled", func(c *Client) {
+		gh, err = c.GetHTTP3(&GetHTTP3Input{
+			ServiceID:      testServiceID,
+			ServiceVersion: tv.Number,
+		})
+	})
+
+	// The API returns a 404 if HTTP3 is not enabled.
+	// The API client returns an error if a non-2xx is returned from the API.
+	if err == nil {
+		t.Fatal("expected a 404 from the API but got a 2xx")
+	}
+}
+
+func TestClient_GetHTTP3_validation(t *testing.T) {
+	var err error
+
+	_, err = testClient.GetHTTP3(&GetHTTP3Input{
+		ServiceVersion: 1,
+	})
+	if err != ErrMissingServiceID {
+		t.Errorf("bad error: %s", err)
+	}
+
+	_, err = testClient.GetHTTP3(&GetHTTP3Input{
+		ServiceID: "foo",
+	})
+	if err != ErrMissingServiceVersion {
+		t.Errorf("bad error: %s", err)
+	}
+}
+
+func TestClient_CreateHTTP3_validation(t *testing.T) {
+	var err error
+	_, err = testClient.EnableHTTP3(&EnableHTTP3Input{
+		ServiceID: "",
+	})
+	if err != ErrMissingServiceID {
+		t.Errorf("bad error: %s", err)
+	}
+
+	_, err = testClient.EnableHTTP3(&EnableHTTP3Input{
+		ServiceID:      "foo",
+		ServiceVersion: 0,
+	})
+	if err != ErrMissingServiceVersion {
+		t.Errorf("bad error: %s", err)
+	}
+}
+
+func TestClient_DeleteHTTP3_validation(t *testing.T) {
+	var err error
+
+	err = testClient.DisableHTTP3(&DisableHTTP3Input{
+		ServiceVersion: 1,
+	})
+	if err != ErrMissingServiceID {
+		t.Errorf("bad error: %s", err)
+	}
+
+	err = testClient.DisableHTTP3(&DisableHTTP3Input{
+		ServiceID: "foo",
+	})
+	if err != ErrMissingServiceVersion {
+		t.Errorf("bad error: %s", err)
+	}
+}
