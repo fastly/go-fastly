eae1d41665f56b190e3b2fffefee75a018d8e00f
Mark McDonnell <Integralist@users.noreply.github.com>
2023-06-14 10:53:29 +0100

    feat(errors): expose rate limit headers (#447)
    * feat(errors): expose rate limit headers
    
    * docs(errors): clarify nil value for rate limit response headers

--- fastly/errors.go
+++ fastly/errors.go
@@ -320,6 +320,14 @@ type HTTPError struct {
 	Errors []*ErrorObject `mapstructure:"errors"`
 	// StatusCode is the HTTP status code (2xx-5xx).
 	StatusCode int
+	// RateLimitRemaining is the number of API requests remaining in the current
+	// rate limit window. A `nil` value indicates the API returned no value for
+	// the associated Fastly-RateLimit-Remaining response header.
+	RateLimitRemaining *int
+	// RateLimitReset is the time at which the current rate limit window resets,
+	// as a Unix timestamp. A `nil` value indicates the API returned no value for
+	// the associated Fastly-RateLimit-Reset response header.
+	RateLimitReset *int
 }
 
 // ErrorObject is a single error.
@@ -343,6 +351,13 @@ type legacyError struct {
 func NewHTTPError(resp *http.Response) *HTTPError {
 	var e HTTPError
 	e.StatusCode = resp.StatusCode
+
+	if v, err := strconv.Atoi(resp.Header.Get("Fastly-RateLimit-Remaining")); err == nil {
+		e.RateLimitRemaining = &v
+	}
+	if v, err := strconv.Atoi(resp.Header.Get("Fastly-RateLimit-Reset")); err == nil {
+		e.RateLimitReset = &v
+	}
 
 	if resp.Body == nil {
 		return &e
@@ -436,6 +451,13 @@ func (e *HTTPError) Error() string {
 		if e.Meta != nil {
 			fmt.Fprintf(&b, "\n    Meta:   %v", *e.Meta)
 		}
+	}
+
+	if e.RateLimitRemaining != nil {
+		fmt.Fprintf(&b, "\n    RateLimitRemaining: %v", *e.RateLimitRemaining)
+	}
+	if e.RateLimitReset != nil {
+		fmt.Fprintf(&b, "\n    RateLimitReset:     %v", *e.RateLimitReset)
 	}
 
 	return b.String()
