ef3b00cd62aef1cfe5c1a77e9eb3733dd1bc2b0a
Integralist <integralist@fastly.com>
2023-11-30 17:31:23 +0000

    fix(token): fix all relevant fields to be pointers

--- fastly/token_test.go
+++ fastly/token_test.go
@@ -1,6 +1,8 @@
 package fastly
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestClient_ListTokens(t *testing.T) {
 	t.Parallel()
@@ -54,10 +56,10 @@ func TestClient_CreateToken(t *testing.T) {
 	t.Parallel()
 
 	input := &CreateTokenInput{
-		Name:     "my-test-token",
-		Scope:    "global",
-		Username: "XXXXXXXXXXXXXXXXXXXXXX",
-		Password: "XXXXXXXXXXXXXXXXXXXXXX",
+		Name:     ToPointer("my-test-token"),
+		Scope:    ToPointer(GlobalScope),
+		Username: ToPointer("XXXXXXXXXXXXXXXXXXXXXX"),
+		Password: ToPointer("XXXXXXXXXXXXXXXXXXXXXX"),
 	}
 
 	var token *Token
@@ -69,12 +71,11 @@ func TestClient_CreateToken(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if token.Name != input.Name {
-		t.Errorf("returned invalid name, got %s, want %s", token.Name, input.Name)
+	if *token.Name != *input.Name {
+		t.Errorf("returned invalid name, got %s, want %s", *token.Name, *input.Name)
 	}
-
-	if token.Scope != input.Scope {
-		t.Errorf("returned invalid scope, got %s, want %s", token.Scope, input.Scope)
+	if *token.Scope != *input.Scope {
+		t.Errorf("returned invalid scope, got %s, want %s", *token.Scope, *input.Scope)
 	}
 }
 
@@ -113,10 +114,10 @@ func TestClient_CreateAndBulkDeleteTokens(t *testing.T) {
 
 	record(t, "tokens/create_and_bulk_delete", func(c *Client) {
 		token1, err := c.CreateToken(&CreateTokenInput{
-			Name:     "my-test-token-1",
-			Scope:    "global",
-			Username: "testing@fastly.com",
-			Password: "foobar",
+			Name:     ToPointer("my-test-token-1"),
+			Scope:    ToPointer(GlobalScope),
+			Username: ToPointer("testing@fastly.com"),
+			Password: ToPointer("foobar"),
 			Services: []string{"0Us63sb8R1BpWQBIhluncu", "7frORaFZvHgC6eRAJdA7kf"},
 		})
 		if err != nil {
@@ -124,10 +125,10 @@ func TestClient_CreateAndBulkDeleteTokens(t *testing.T) {
 		}
 
 		token2, err := c.CreateToken(&CreateTokenInput{
-			Name:     "my-test-token-2",
-			Scope:    "global",
-			Username: "testing@fastly.com",
-			Password: "foobar",
+			Name:     ToPointer("my-test-token-2"),
+			Scope:    ToPointer(GlobalScope),
+			Username: ToPointer("testing@fastly.com"),
+			Password: ToPointer("foobar"),
 			Services: []string{"0Us63sb8R1BpWQBIhluncu", "7frORaFZvHgC6eRAJdA7kf"},
 		})
 		if err != nil {
@@ -136,8 +137,8 @@ func TestClient_CreateAndBulkDeleteTokens(t *testing.T) {
 
 		deleteErr = c.BatchDeleteTokens(&BatchDeleteTokensInput{
 			Tokens: []*BatchToken{
-				{ID: token1.ID},
-				{ID: token2.ID},
+				{ID: *token1.ID},
+				{ID: *token2.ID},
 			},
 		})
 	})
