a5ce508f262ab599c9ea2356c41fb77cf36e748b
Integralist <integralist@fastly.com>
2023-11-30 15:37:58 +0000

    fix(s3): fix all relevant fields to be pointers

--- fastly/s3_test.go
+++ fastly/s3_test.go
@@ -244,107 +244,107 @@ func TestClient_S3s(t *testing.T) {
 		})
 	}()
 
-	if s3CreateResp1.Name != "test-s3" {
-		t.Errorf("bad name: %q", s3CreateResp1.Name)
+	if *s3CreateResp1.Name != "test-s3" {
+		t.Errorf("bad name: %q", *s3CreateResp1.Name)
 	}
-	if s3CreateResp1.BucketName != "bucket-name" {
-		t.Errorf("bad bucket_name: %q", s3CreateResp1.BucketName)
+	if *s3CreateResp1.BucketName != "bucket-name" {
+		t.Errorf("bad bucket_name: %q", *s3CreateResp1.BucketName)
 	}
-	if s3CreateResp1.AccessKey != "AKIAIOSFODNN7EXAMPLE" {
-		t.Errorf("bad access_key: %q", s3CreateResp1.AccessKey)
+	if *s3CreateResp1.AccessKey != "AKIAIOSFODNN7EXAMPLE" {
+		t.Errorf("bad access_key: %q", *s3CreateResp1.AccessKey)
 	}
-	if s3CreateResp1.SecretKey != "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" {
-		t.Errorf("bad secret_key: %q", s3CreateResp1.SecretKey)
+	if *s3CreateResp1.SecretKey != "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" {
+		t.Errorf("bad secret_key: %q", *s3CreateResp1.SecretKey)
 	}
-	if s3CreateResp1.IAMRole != "" {
-		t.Errorf("bad iam_role: %q", s3CreateResp1.IAMRole)
+	if s3CreateResp1.IAMRole != nil {
+		t.Errorf("bad iam_role: %q", *s3CreateResp1.IAMRole)
 	}
-	if s3CreateResp1.Domain != "s3.us-east-1.amazonaws.com" {
-		t.Errorf("bad domain: %q", s3CreateResp1.Domain)
+	if *s3CreateResp1.Domain != "s3.us-east-1.amazonaws.com" {
+		t.Errorf("bad domain: %q", *s3CreateResp1.Domain)
 	}
-	if s3CreateResp1.Path != "/path" {
-		t.Errorf("bad path: %q", s3CreateResp1.Path)
+	if *s3CreateResp1.Path != "/path" {
+		t.Errorf("bad path: %q", *s3CreateResp1.Path)
 	}
-	if s3CreateResp1.Period != 12 {
-		t.Errorf("bad period: %q", s3CreateResp1.Period)
+	if *s3CreateResp1.Period != 12 {
+		t.Errorf("bad period: %q", *s3CreateResp1.Period)
 	}
-	if s3CreateResp1.CompressionCodec != "snappy" {
-		t.Errorf("bad compression_codec: %q", s3CreateResp1.CompressionCodec)
+	if *s3CreateResp1.CompressionCodec != "snappy" {
+		t.Errorf("bad compression_codec: %q", *s3CreateResp1.CompressionCodec)
 	}
-	if s3CreateResp1.GzipLevel != 0 {
-		t.Errorf("bad gzip_level: %q", s3CreateResp1.GzipLevel)
+	if *s3CreateResp1.GzipLevel != 0 {
+		t.Errorf("bad gzip_level: %q", *s3CreateResp1.GzipLevel)
 	}
-	if s3CreateResp1.Format != "format" {
-		t.Errorf("bad format: %q", s3CreateResp1.Format)
+	if *s3CreateResp1.Format != "format" {
+		t.Errorf("bad format: %q", *s3CreateResp1.Format)
 	}
-	if s3CreateResp1.FormatVersion != 2 {
-		t.Errorf("bad format_version: %q", s3CreateResp1.FormatVersion)
+	if *s3CreateResp1.FormatVersion != 2 {
+		t.Errorf("bad format_version: %q", *s3CreateResp1.FormatVersion)
 	}
-	if s3CreateResp1.TimestampFormat != "%Y" {
-		t.Errorf("bad timestamp_format: %q", s3CreateResp1.TimestampFormat)
+	if *s3CreateResp1.TimestampFormat != "%Y" {
+		t.Errorf("bad timestamp_format: %q", *s3CreateResp1.TimestampFormat)
 	}
-	if s3CreateResp1.Redundancy != S3RedundancyReduced {
-		t.Errorf("bad redundancy: %q", s3CreateResp1.Redundancy)
+	if *s3CreateResp1.Redundancy != S3RedundancyReduced {
+		t.Errorf("bad redundancy: %q", *s3CreateResp1.Redundancy)
 	}
-	if s3CreateResp1.MessageType != "classic" {
-		t.Errorf("bad message_type: %q", s3CreateResp1.MessageType)
+	if *s3CreateResp1.MessageType != "classic" {
+		t.Errorf("bad message_type: %q", *s3CreateResp1.MessageType)
 	}
-	if s3CreateResp1.Placement != "waf_debug" {
-		t.Errorf("bad placement: %q", s3CreateResp1.Placement)
+	if *s3CreateResp1.Placement != "waf_debug" {
+		t.Errorf("bad placement: %q", *s3CreateResp1.Placement)
 	}
-	if s3CreateResp1.ResponseCondition != "" {
-		t.Errorf("bad response_condition: %q", s3CreateResp1.ResponseCondition)
+	if *s3CreateResp1.ResponseCondition != "" {
+		t.Errorf("bad response_condition: %q", *s3CreateResp1.ResponseCondition)
 	}
-	if s3CreateResp1.PublicKey != pgpPublicKey() {
-		t.Errorf("bad public_key: %q", s3CreateResp1.PublicKey)
+	if *s3CreateResp1.PublicKey != pgpPublicKey() {
+		t.Errorf("bad public_key: %q", *s3CreateResp1.PublicKey)
 	}
-	if s3CreateResp1.ServerSideEncryption != S3ServerSideEncryptionKMS {
-		t.Errorf("bad server_side_encryption: %q", s3CreateResp1.ServerSideEncryption)
+	if *s3CreateResp1.ServerSideEncryption != S3ServerSideEncryptionKMS {
+		t.Errorf("bad server_side_encryption: %q", *s3CreateResp1.ServerSideEncryption)
 	}
-	if s3CreateResp1.ServerSideEncryptionKMSKeyID != "1234" {
-		t.Errorf("bad server_side_encryption_kms_key_id: %q", s3CreateResp1.ServerSideEncryptionKMSKeyID)
+	if *s3CreateResp1.ServerSideEncryptionKMSKeyID != "1234" {
+		t.Errorf("bad server_side_encryption_kms_key_id: %q", *s3CreateResp1.ServerSideEncryptionKMSKeyID)
 	}
-	if s3CreateResp1.ACL != S3AccessControlListPrivate {
-		t.Errorf("bad acl: %s", s3CreateResp1.ACL)
+	if *s3CreateResp1.ACL != S3AccessControlListPrivate {
+		t.Errorf("bad acl: %s", *s3CreateResp1.ACL)
 	}
-	if s3CreateResp1.FileMaxBytes != MiB {
-		t.Errorf("bad file_max_bytes: %q", s3CreateResp1.FileMaxBytes)
+	if *s3CreateResp1.FileMaxBytes != MiB {
+		t.Errorf("bad file_max_bytes: %q", *s3CreateResp1.FileMaxBytes)
 	}
-	if s3CreateResp2.FileMaxBytes != 10*MiB {
-		t.Errorf("bad file_max_bytes: %q", s3CreateResp2.FileMaxBytes)
+	if *s3CreateResp2.FileMaxBytes != 10*MiB {
+		t.Errorf("bad file_max_bytes: %q", *s3CreateResp2.FileMaxBytes)
 	}
-	if s3CreateResp3.CompressionCodec != "snappy" {
-		t.Errorf("bad compression_codec: %q", s3CreateResp1.CompressionCodec)
+	if *s3CreateResp3.CompressionCodec != "snappy" {
+		t.Errorf("bad compression_codec: %q", *s3CreateResp1.CompressionCodec)
 	}
-	if s3CreateResp3.GzipLevel != 0 {
-		t.Errorf("bad gzip_level: %q", s3CreateResp1.GzipLevel)
+	if *s3CreateResp3.GzipLevel != 0 {
+		t.Errorf("bad gzip_level: %q", *s3CreateResp1.GzipLevel)
 	}
-	if s3CreateResp3.Redundancy != S3RedundancyStandardIA {
-		t.Errorf("bad acl: %s", s3CreateResp3.Redundancy)
+	if *s3CreateResp3.Redundancy != S3RedundancyStandardIA {
+		t.Errorf("bad acl: %s", *s3CreateResp3.Redundancy)
 	}
-	if s3CreateResp3.ACL != S3AccessControlListBucketOwnerFullControl {
-		t.Errorf("bad acl: %s", s3CreateResp3.ACL)
+	if *s3CreateResp3.ACL != S3AccessControlListBucketOwnerFullControl {
+		t.Errorf("bad acl: %s", *s3CreateResp3.ACL)
 	}
-	if s3CreateResp3.FileMaxBytes != 0 {
-		t.Errorf("bad file_max_bytes: %q", s3CreateResp3.FileMaxBytes)
+	if *s3CreateResp3.FileMaxBytes != 0 {
+		t.Errorf("bad file_max_bytes: %q", *s3CreateResp3.FileMaxBytes)
 	}
-	if s3CreateResp4.AccessKey != "" {
-		t.Errorf("bad access_key: %q", s3CreateResp4.AccessKey)
+	if s3CreateResp4.AccessKey != nil {
+		t.Errorf("bad access_key: %q", *s3CreateResp4.AccessKey)
 	}
-	if s3CreateResp4.SecretKey != "" {
-		t.Errorf("bad secret_key: %q", s3CreateResp4.SecretKey)
+	if s3CreateResp4.SecretKey != nil {
+		t.Errorf("bad secret_key: %q", *s3CreateResp4.SecretKey)
 	}
-	if s3CreateResp4.IAMRole != "arn:aws:iam::123456789012:role/S3Access" {
-		t.Errorf("bad iam_role: %q", s3CreateResp4.IAMRole)
+	if *s3CreateResp4.IAMRole != "arn:aws:iam::123456789012:role/S3Access" {
+		t.Errorf("bad iam_role: %q", *s3CreateResp4.IAMRole)
 	}
-	if s3CreateResp4.Redundancy != S3RedundancyStandard {
-		t.Errorf("bad acl: %s", s3CreateResp4.Redundancy)
+	if *s3CreateResp4.Redundancy != S3RedundancyStandard {
+		t.Errorf("bad acl: %s", *s3CreateResp4.Redundancy)
 	}
-	if s3CreateResp4.ACL != "" {
-		t.Errorf("bad acl: %s", s3CreateResp4.ACL)
+	if s3CreateResp4.ACL != nil {
+		t.Errorf("bad acl: %s", *s3CreateResp4.ACL)
 	}
-	if s3CreateResp4.FileMaxBytes != 10*MiB {
-		t.Errorf("bad file_max_bytes: %q", s3CreateResp4.FileMaxBytes)
+	if *s3CreateResp4.FileMaxBytes != 10*MiB {
+		t.Errorf("bad file_max_bytes: %q", *s3CreateResp4.FileMaxBytes)
 	}
 
 	// List
@@ -387,80 +387,80 @@ func TestClient_S3s(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if s3CreateResp1.Name != s3GetResp.Name {
-		t.Errorf("bad name: %q", s3CreateResp1.Name)
+	if *s3CreateResp1.Name != *s3GetResp.Name {
+		t.Errorf("bad name: %q", *s3CreateResp1.Name)
 	}
-	if s3CreateResp1.BucketName != s3GetResp.BucketName {
-		t.Errorf("bad bucket_name: %q", s3CreateResp1.BucketName)
+	if *s3CreateResp1.BucketName != *s3GetResp.BucketName {
+		t.Errorf("bad bucket_name: %q", *s3CreateResp1.BucketName)
 	}
-	if s3CreateResp1.AccessKey != s3GetResp.AccessKey {
-		t.Errorf("bad access_key: %q", s3CreateResp1.AccessKey)
+	if *s3CreateResp1.AccessKey != *s3GetResp.AccessKey {
+		t.Errorf("bad access_key: %q", *s3CreateResp1.AccessKey)
 	}
-	if s3CreateResp1.SecretKey != s3GetResp.SecretKey {
-		t.Errorf("bad secret_key: %q", s3CreateResp1.SecretKey)
+	if *s3CreateResp1.SecretKey != *s3GetResp.SecretKey {
+		t.Errorf("bad secret_key: %q", *s3CreateResp1.SecretKey)
 	}
 	if s3CreateResp1.IAMRole != s3GetResp.IAMRole {
-		t.Errorf("bad iam_role: %q", s3CreateResp1.IAMRole)
+		t.Errorf("bad iam_role: %q", *s3CreateResp1.IAMRole)
 	}
-	if s3CreateResp1.Domain != s3GetResp.Domain {
-		t.Errorf("bad domain: %q", s3CreateResp1.Domain)
+	if *s3CreateResp1.Domain != *s3GetResp.Domain {
+		t.Errorf("bad domain: %q", *s3CreateResp1.Domain)
 	}
-	if s3CreateResp1.Path != s3GetResp.Path {
-		t.Errorf("bad path: %q", s3CreateResp1.Path)
+	if *s3CreateResp1.Path != *s3GetResp.Path {
+		t.Errorf("bad path: %q", *s3CreateResp1.Path)
 	}
-	if s3CreateResp1.Period != s3GetResp.Period {
-		t.Errorf("bad period: %q", s3CreateResp1.Period)
+	if *s3CreateResp1.Period != *s3GetResp.Period {
+		t.Errorf("bad period: %q", *s3CreateResp1.Period)
 	}
-	if s3CreateResp1.CompressionCodec != s3GetResp.CompressionCodec {
-		t.Errorf("bad compression_codec: %q", s3CreateResp1.CompressionCodec)
+	if *s3CreateResp1.CompressionCodec != *s3GetResp.CompressionCodec {
+		t.Errorf("bad compression_codec: %q", *s3CreateResp1.CompressionCodec)
 	}
-	if s3CreateResp1.GzipLevel != s3GetResp.GzipLevel {
-		t.Errorf("bad gzip_level: %q", s3CreateResp1.GzipLevel)
+	if *s3CreateResp1.GzipLevel != *s3GetResp.GzipLevel {
+		t.Errorf("bad gzip_level: %q", *s3CreateResp1.GzipLevel)
 	}
-	if s3CreateResp1.Format != s3GetResp.Format {
-		t.Errorf("bad format: %q", s3CreateResp1.Format)
+	if *s3CreateResp1.Format != *s3GetResp.Format {
+		t.Errorf("bad format: %q", *s3CreateResp1.Format)
 	}
-	if s3CreateResp1.FormatVersion != s3GetResp.FormatVersion {
-		t.Errorf("bad format_version: %q", s3CreateResp1.FormatVersion)
+	if *s3CreateResp1.FormatVersion != *s3GetResp.FormatVersion {
+		t.Errorf("bad format_version: %q", *s3CreateResp1.FormatVersion)
 	}
-	if s3CreateResp1.TimestampFormat != s3GetResp.TimestampFormat {
-		t.Errorf("bad timestamp_format: %q", s3CreateResp1.TimestampFormat)
+	if *s3CreateResp1.TimestampFormat != *s3GetResp.TimestampFormat {
+		t.Errorf("bad timestamp_format: %q", *s3CreateResp1.TimestampFormat)
 	}
-	if s3CreateResp1.Redundancy != s3GetResp.Redundancy {
-		t.Errorf("bad redundancy: %q", s3CreateResp1.Redundancy)
+	if *s3CreateResp1.Redundancy != *s3GetResp.Redundancy {
+		t.Errorf("bad redundancy: %q", *s3CreateResp1.Redundancy)
 	}
-	if s3CreateResp1.Placement != s3GetResp.Placement {
-		t.Errorf("bad placement: %q", s3CreateResp1.Placement)
+	if *s3CreateResp1.Placement != *s3GetResp.Placement {
+		t.Errorf("bad placement: %q", *s3CreateResp1.Placement)
 	}
-	if s3CreateResp1.ResponseCondition != "" {
-		t.Errorf("bad response_condition: %q", s3CreateResp1.ResponseCondition)
+	if *s3CreateResp1.ResponseCondition != "" {
+		t.Errorf("bad response_condition: %q", *s3CreateResp1.ResponseCondition)
 	}
-	if s3CreateResp1.PublicKey != pgpPublicKey() {
-		t.Errorf("bad public_key: %q", s3CreateResp1.PublicKey)
+	if *s3CreateResp1.PublicKey != pgpPublicKey() {
+		t.Errorf("bad public_key: %q", *s3CreateResp1.PublicKey)
 	}
-	if s3CreateResp1.ServerSideEncryption != s3GetResp.ServerSideEncryption {
-		t.Errorf("bad server_side_encryption: %q", s3CreateResp1.ServerSideEncryption)
+	if *s3CreateResp1.ServerSideEncryption != *s3GetResp.ServerSideEncryption {
+		t.Errorf("bad server_side_encryption: %q", *s3CreateResp1.ServerSideEncryption)
 	}
-	if s3CreateResp1.ServerSideEncryptionKMSKeyID != s3GetResp.ServerSideEncryptionKMSKeyID {
-		t.Errorf("bad server_side_encryption_kms_key_id: %q", s3CreateResp1.ServerSideEncryptionKMSKeyID)
+	if *s3CreateResp1.ServerSideEncryptionKMSKeyID != *s3GetResp.ServerSideEncryptionKMSKeyID {
+		t.Errorf("bad server_side_encryption_kms_key_id: %q", *s3CreateResp1.ServerSideEncryptionKMSKeyID)
 	}
-	if s3CreateResp1.ACL != s3GetResp.ACL {
-		t.Errorf("bad acl: %s", s3CreateResp1.ACL)
+	if *s3CreateResp1.ACL != *s3GetResp.ACL {
+		t.Errorf("bad acl: %s", *s3CreateResp1.ACL)
 	}
 	if s3CreateResp4.AccessKey != s3GetResp2.AccessKey {
-		t.Errorf("bad access_key: %q", s3CreateResp4.AccessKey)
+		t.Errorf("bad access_key: %q", *s3CreateResp4.AccessKey)
 	}
 	if s3CreateResp4.SecretKey != s3GetResp2.SecretKey {
-		t.Errorf("bad secret_key: %q", s3CreateResp4.SecretKey)
+		t.Errorf("bad secret_key: %q", *s3CreateResp4.SecretKey)
 	}
-	if s3CreateResp4.IAMRole != s3GetResp2.IAMRole {
-		t.Errorf("bad iam_role: %q", s3CreateResp4.IAMRole)
+	if *s3CreateResp4.IAMRole != *s3GetResp2.IAMRole {
+		t.Errorf("bad iam_role: %q", *s3CreateResp4.IAMRole)
 	}
-	if s3CreateResp4.Redundancy != s3GetResp2.Redundancy {
-		t.Errorf("bad redundancy: %q", s3CreateResp4.Redundancy)
+	if *s3CreateResp4.Redundancy != *s3GetResp2.Redundancy {
+		t.Errorf("bad redundancy: %q", *s3CreateResp4.Redundancy)
 	}
 	if s3CreateResp4.ACL != s3GetResp2.ACL {
-		t.Errorf("bad acl: %s", s3CreateResp4.ACL)
+		t.Errorf("bad acl: %s", *s3CreateResp4.ACL)
 	}
 
 	// Update
@@ -554,50 +554,50 @@ func TestClient_S3s(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if s3UpdateResp1.Name != "new-test-s3" {
-		t.Errorf("bad name: %q", s3UpdateResp1.Name)
+	if *s3UpdateResp1.Name != "new-test-s3" {
+		t.Errorf("bad name: %q", *s3UpdateResp1.Name)
 	}
-	if s3UpdateResp1.PublicKey != pgpPublicKeyUpdate() {
-		t.Errorf("bad public_key: %q", s3UpdateResp1.PublicKey)
+	if *s3UpdateResp1.PublicKey != pgpPublicKeyUpdate() {
+		t.Errorf("bad public_key: %q", *s3UpdateResp1.PublicKey)
 	}
-	if s3UpdateResp1.CompressionCodec != "zstd" {
-		t.Errorf("bad compression_codec: %q", s3UpdateResp1.CompressionCodec)
+	if *s3UpdateResp1.CompressionCodec != "zstd" {
+		t.Errorf("bad compression_codec: %q", *s3UpdateResp1.CompressionCodec)
 	}
-	if s3UpdateResp1.GzipLevel != 0 {
-		t.Errorf("bad gzip_level: %q", s3UpdateResp1.GzipLevel)
+	if s3UpdateResp1.GzipLevel != nil {
+		t.Errorf("bad gzip_level: %q", *s3UpdateResp1.GzipLevel)
 	}
-	if s3UpdateResp1.FileMaxBytes != 5*MiB {
-		t.Errorf("bad file_max_bytes: %q", s3UpdateResp1.FileMaxBytes)
+	if *s3UpdateResp1.FileMaxBytes != 5*MiB {
+		t.Errorf("bad file_max_bytes: %q", *s3UpdateResp1.FileMaxBytes)
 	}
-	if s3UpdateResp2.CompressionCodec != "zstd" {
-		t.Errorf("bad compression_codec: %q", s3UpdateResp2.CompressionCodec)
+	if *s3UpdateResp2.CompressionCodec != "zstd" {
+		t.Errorf("bad compression_codec: %q", *s3UpdateResp2.CompressionCodec)
 	}
-	if s3UpdateResp2.GzipLevel != 0 {
-		t.Errorf("bad gzip_level: %q", s3UpdateResp2.GzipLevel)
+	if *s3UpdateResp2.GzipLevel != 0 {
+		t.Errorf("bad gzip_level: %q", *s3UpdateResp2.GzipLevel)
 	}
-	if s3UpdateResp3.CompressionCodec != "" {
-		t.Errorf("bad compression_codec: %q", s3UpdateResp3.CompressionCodec)
+	if s3UpdateResp3.CompressionCodec != nil {
+		t.Errorf("bad compression_codec: %q", *s3UpdateResp3.CompressionCodec)
 	}
-	if s3UpdateResp3.GzipLevel != 9 {
-		t.Errorf("bad gzip_level: %q", s3UpdateResp3.GzipLevel)
+	if *s3UpdateResp3.GzipLevel != 9 {
+		t.Errorf("bad gzip_level: %q", *s3UpdateResp3.GzipLevel)
 	}
-	if s3UpdateResp4.AccessKey != "" {
-		t.Errorf("bad access_key: %q", s3UpdateResp4.AccessKey)
+	if *s3UpdateResp4.AccessKey != "" {
+		t.Errorf("bad access_key: %q", *s3UpdateResp4.AccessKey)
 	}
-	if s3UpdateResp4.SecretKey != "" {
-		t.Errorf("bad secret_key: %q", s3UpdateResp4.SecretKey)
+	if *s3UpdateResp4.SecretKey != "" {
+		t.Errorf("bad secret_key: %q", *s3UpdateResp4.SecretKey)
 	}
-	if s3UpdateResp4.IAMRole != "arn:aws:iam::123456789012:role/S3Access" {
-		t.Errorf("bad iam_role: %q", s3UpdateResp4.IAMRole)
+	if *s3UpdateResp4.IAMRole != "arn:aws:iam::123456789012:role/S3Access" {
+		t.Errorf("bad iam_role: %q", *s3UpdateResp4.IAMRole)
 	}
-	if s3UpdateResp5.AccessKey != "AKIAIOSFODNN7EXAMPLE" {
-		t.Errorf("bad access_key: %q", s3UpdateResp5.AccessKey)
+	if *s3UpdateResp5.AccessKey != "AKIAIOSFODNN7EXAMPLE" {
+		t.Errorf("bad access_key: %q", *s3UpdateResp5.AccessKey)
 	}
-	if s3UpdateResp5.SecretKey != "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" {
-		t.Errorf("bad secret_key: %q", s3UpdateResp5.SecretKey)
+	if *s3UpdateResp5.SecretKey != "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" {
+		t.Errorf("bad secret_key: %q", *s3UpdateResp5.SecretKey)
 	}
-	if s3UpdateResp5.IAMRole != "" {
-		t.Errorf("bad iam_role: %q", s3UpdateResp5.IAMRole)
+	if *s3UpdateResp5.IAMRole != "" {
+		t.Errorf("bad iam_role: %q", *s3UpdateResp5.IAMRole)
 	}
 
 	// Delete
