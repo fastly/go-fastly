f468534e7435852848534622a55781c1981dd80d
Integralist <integralist@fastly.com>
2023-12-01 11:49:34 +0000

    feat(helper): add ToValue generic function

--- fastly/basictypes_helper.go
+++ fastly/basictypes_helper.go
@@ -5,6 +5,16 @@ func ToPointer[T ~string | ~int | int32 | ~int64 | uint | uint8 | uint32 | ~bool
 	return &v
 }
 
+// ToValue converts *T to T.
+// If v is nil, then return T's zero value.
+func ToValue[T ~string | ~int | int32 | ~int64 | uint | uint8 | uint32 | ~bool](v *T) T {
+	if v != nil {
+		return *v
+	}
+	var zero T
+	return zero
+}
+
 // NullString is a helper that returns a pointer to the string value passed in
 // or nil if the string is empty.
 //
