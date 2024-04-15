6d0ba1d8c9698836e27b4facb73f489bde38e4fb
Dabo Ross <dross@fastly.com>
2024-04-15 11:45:03 -0700

    Add API for new Image Optimizer default settings API

---
+++ fastly/image_optimizer_default_settings.go
@@ -0,0 +1,140 @@
+package fastly
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// ResizeFilter is a base for the different ResizeFilter variants.
+type ResizeFilter int64
+
+func (r ResizeFilter) String() string {
+	switch r {
+	case Lanczos3:
+		return "lanczos3"
+	case Lanczos2:
+		return "lanczos2"
+	case Bicubic:
+		return "bicubic"
+	case Bilinear:
+		return "bilinear"
+	case Nearest:
+		return "nearest"
+	}
+	return "lanczos3" // default
+}
+
+const (
+	Lanczos3 ResizeFilter = iota
+	Lanczos2
+	Bicubic
+	Bilinear
+	Nearest
+)
+
+// ImageOptimizerDefaultSettings represents the returned default settings for Image Optimizer on a given service.
+type ImageOptimizerDefaultSettings struct {
+	// Type of filter to use while resizing an image.
+	ResizeFilter string `json:"resize_filter"`
+	// Whether or not to default to webp output when the client supports it. This is equivalent to adding "auto=webp" to all image optimizer requests.
+	Webp bool `json:"webp"`
+	// Default quality to use with webp output. This can be overriden with the second option in the "quality" URL parameter on specific image optimizer
+	// requests.
+	WebpQuality int `json:"webp_quality"`
+	// Default type of jpeg output to use. This can be overriden with "format=bjpeg" and "format=pjpeg" on specific image optimizer requests.
+	JpegType string `json:"jpeg_type"`
+	// Default quality to use with jpeg output. This can be overridden with the "quality" parameter on specific image optimizer requests.
+	JpegQuality int `json:"jpeg_quality"`
+	// Whether or not we should allow output images to render at sizes larger than input.
+	Upscale bool `json:"upscale"`
+	// Enables GIF to MP4 transformations on this service.
+	AllowVideo bool `json:"allow_video"`
+}
+
+// GetImageOptimizerDefaultSettingsInput is used as input to the
+// GetImageOptimizerDefaultSettings function.
+type GetImageOptimizerDefaultSettingsInput struct {
+	// ServiceID is the ID of the service (required).
+	ServiceID string
+	// ServiceVersion is the specific configuration version (required).
+	ServiceVersion int
+}
+
+// UpdateImageOptimizerDefaultSettingsInput is used as input to the
+// UpdateImageOptimizerDefaultSettings function.
+//
+// A minimum of one optional field is required.
+type UpdateImageOptimizerDefaultSettingsInput struct {
+	// ServiceID is the ID of the service (required).
+	ServiceID string `json:"-"`
+	// ServiceVersion is the specific configuration version (required).
+	ServiceVersion int `json:"-"`
+	// Type of filter to use while resizing an image.
+	ResizeFilter *ResizeFilter `json:"resize_filter,omitempty"`
+	// Whether or not to default to webp output when the client supports it. This is equivalent to adding "auto=webp" to all image optimizer requests.
+	Webp *bool `json:"webp,omitempty"`
+	// Default quality to use with webp output. This can be overriden with the second option in the "quality" URL parameter on specific image optimizer
+	// requests.
+	WebpQuality *int `json:"webp_quality,omitempty"`
+	// Default type of jpeg output to use. This can be overriden with "format=bjpeg" and "format=pjpeg" on specific image optimizer requests.
+	JpegType *string `json:"jpeg_type,omitempty"`
+	// Default quality to use with jpeg output. This can be overridden with the "quality" parameter on specific image optimizer requests.
+	JpegQuality *int `json:"jpeg_quality,omitempty"`
+	// Whether or not we should allow output images to render at sizes larger than input.
+	Upscale *bool `json:"upscale,omitempty"`
+	// Enables GIF to MP4 transformations on this service.
+	AllowVideo *bool `json:"allow_video,omitempty"`
+}
+
+// GetImageOptimizerDefaultSettings retrives the current Image Optimizer default settings on a given service version.
+func (c *Client) GetImageOptimizerDefaultSettings(i *GetImageOptimizerDefaultSettingsInput) (*ImageOptimizerDefaultSettings, error) {
+	if i.ServiceID == "" {
+		return nil, ErrMissingServiceID
+	}
+	if i.ServiceVersion == 0 {
+		return nil, ErrMissingServiceVersion
+	}
+
+	path := fmt.Sprintf("/service/%s/version/%d/image_optimizer_default_settings", i.ServiceID, i.ServiceVersion)
+
+	resp, err := c.Get(path, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var iods *ImageOptimizerDefaultSettings
+	if err := json.NewDecoder(resp.Body).Decode(&iods); err != nil {
+		return nil, err
+	}
+
+	return iods, nil
+}
+
+// UpdateImageOptimizerDefaultSettings changes one or more Image Optimizer default settings on a given service version.
+func (c *Client) UpdateImageOptimizerDefaultSettings(i *UpdateImageOptimizerDefaultSettingsInput) (*ImageOptimizerDefaultSettings, error) {
+	if i.ServiceID == "" {
+		return nil, ErrMissingServiceID
+	}
+	if i.ServiceVersion == 0 {
+		return nil, ErrMissingServiceVersion
+	}
+	if i.ResizeFilter == nil && i.Webp == nil && i.WebpQuality == nil && i.JpegType == nil && i.JpegQuality == nil && i.Upscale == nil && i.AllowVideo == nil {
+		return nil, ErrMissingImageOptimizerDefaultSetting
+	}
+
+	path := fmt.Sprintf("/service/%s/version/%d/image_optimizer_default_settings", i.ServiceID, i.ServiceVersion)
+
+	resp, err := c.PatchJSON(path, i, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var iods *ImageOptimizerDefaultSettings
+	if err := json.NewDecoder(resp.Body).Decode(&iods); err != nil {
+		return nil, err
+	}
+
+	return iods, nil
+}
---
+++ fastly/image_optimizer_default_settings_test.go
@@ -0,0 +1,235 @@
+package fastly
+
+import (
+	"strings"
+	"testing"
+)
+
+// TestClient_ImageOptimizerDefaultSettings tests the Image Optimizer Default Settings API
+//
+// Note: to run this, all backends on the active version of the test service must have shielding
+// enabled, and the test service's account must be allowed to enable Image Optimization.
+func TestClient_ImageOptimizerDefaultSettings(t *testing.T) {
+	t.Parallel()
+
+	fixtureBase := "image_optimizer_default_settings/"
+
+	testVersion := createTestVersion(t, fixtureBase+"version", testServiceID)
+
+	var err error
+
+	// Enable IO
+	record(t, fixtureBase+"enable_product", func(c *Client) {
+		_, err = c.EnableProduct(&ProductEnablementInput{
+			ProductID: ProductImageOptimizer,
+			ServiceID: testServiceID,
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Ensure we disable IO on the service after the test
+	defer func() {
+		record(t, fixtureBase+"disable_product", func(c *Client) {
+			c.DisableProduct(&ProductEnablementInput{
+				ProductID: ProductImageOptimizer,
+				ServiceID: testServiceID,
+			})
+		})
+	}()
+
+	var defaultSettings *ImageOptimizerDefaultSettings
+
+	// Fetch
+	record(t, fixtureBase+"original_fetch", func(c *Client) {
+		defaultSettings, err = c.GetImageOptimizerDefaultSettings(&GetImageOptimizerDefaultSettingsInput{
+			ServiceID:      testServiceID,
+			ServiceVersion: *testVersion.Number,
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	originalSettings := defaultSettings
+
+	// Reset our settings back to the original
+	defer func() {
+		record(t, fixtureBase+"final_reset", func(c *Client) {
+			_, err = c.UpdateImageOptimizerDefaultSettings(&UpdateImageOptimizerDefaultSettingsInput{
+				ServiceID:      testServiceID,
+				ServiceVersion: *testVersion.Number,
+				Webp:           &originalSettings.Webp,
+				WebpQuality:    &originalSettings.WebpQuality,
+				Upscale:        &originalSettings.Upscale,
+			})
+		})
+	}()
+
+	newWebp := false
+	newWebpQuality := 20
+	newUpscale := false
+
+	// Change some stuff
+	record(t, fixtureBase+"update_1_patch", func(c *Client) {
+		defaultSettings, err = c.UpdateImageOptimizerDefaultSettings(&UpdateImageOptimizerDefaultSettingsInput{
+			ServiceID:      testServiceID,
+			ServiceVersion: *testVersion.Number,
+			Webp:           &newWebp,
+			WebpQuality:    &newWebpQuality,
+			Upscale:        &newUpscale,
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if defaultSettings.Webp != newWebp {
+		t.Fatalf("expected Webp: %v; got: %v", newWebp, defaultSettings.Webp)
+	}
+	if defaultSettings.WebpQuality != newWebpQuality {
+		t.Fatalf("expected WebpQuality %v; got: %v", newWebp, defaultSettings.WebpQuality)
+	}
+	if defaultSettings.Upscale != newUpscale {
+		t.Fatalf("expected Upscale: %v; got: %v", newUpscale, defaultSettings.Webp)
+	}
+
+	// Confirm our changes were applied permanently
+	record(t, fixtureBase+"update_1_get", func(c *Client) {
+		defaultSettings, err = c.GetImageOptimizerDefaultSettings(&GetImageOptimizerDefaultSettingsInput{
+			ServiceID:      testServiceID,
+			ServiceVersion: *testVersion.Number,
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if defaultSettings.Webp != newWebp {
+		t.Fatalf("expected Webp: %v; got: %v", newWebp, defaultSettings.Webp)
+	}
+	if defaultSettings.WebpQuality != newWebpQuality {
+		t.Fatalf("expected WebpQuality %v; got: %v", newWebp, defaultSettings.WebpQuality)
+	}
+	if defaultSettings.Upscale != newUpscale {
+		t.Fatalf("expected Upscale: %v; got: %v", newUpscale, defaultSettings.Webp)
+	}
+
+	// Change settings again (to ensure we don't accidentally pass a test by just having the settings already set)
+	newWebp = true
+	newWebpQuality = 42
+	newUpscale = true
+
+	record(t, fixtureBase+"update_2_patch", func(c *Client) {
+		defaultSettings, err = c.UpdateImageOptimizerDefaultSettings(&UpdateImageOptimizerDefaultSettingsInput{
+			ServiceID:      testServiceID,
+			ServiceVersion: *testVersion.Number,
+			Webp:           &newWebp,
+			WebpQuality:    &newWebpQuality,
+			Upscale:        &newUpscale,
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if defaultSettings.Webp != newWebp {
+		t.Fatalf("expected Webp: %v; got: %v", newWebp, defaultSettings.Webp)
+	}
+	if defaultSettings.WebpQuality != newWebpQuality {
+		t.Fatalf("expected WebpQuality %v; got: %v", newWebp, defaultSettings.WebpQuality)
+	}
+	if defaultSettings.Upscale != newUpscale {
+		t.Fatalf("expected Upscale: %v; got: %v", newUpscale, defaultSettings.Webp)
+	}
+
+	// Confirm our changes were applied permanently (again)
+	record(t, fixtureBase+"update_2_get", func(c *Client) {
+		defaultSettings, err = c.GetImageOptimizerDefaultSettings(&GetImageOptimizerDefaultSettingsInput{
+			ServiceID:      testServiceID,
+			ServiceVersion: *testVersion.Number,
+		})
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if defaultSettings.Webp != newWebp {
+		t.Fatalf("expected Webp: %v; got: %v", newWebp, defaultSettings.Webp)
+	}
+	if defaultSettings.WebpQuality != newWebpQuality {
+		t.Fatalf("expected WebpQuality %v; got: %v", newWebp, defaultSettings.WebpQuality)
+	}
+	if defaultSettings.Upscale != newUpscale {
+		t.Fatalf("expected Upscale: %v; got: %v", newUpscale, defaultSettings.Webp)
+	}
+
+	// Apply a setting that produces a server-side error, and confirm it's handled well.
+	newWebpQuality = 105
+
+	record(t, fixtureBase+"incorrect_fetch", func(c *Client) {
+		_, err = c.UpdateImageOptimizerDefaultSettings(&UpdateImageOptimizerDefaultSettingsInput{
+			ServiceID:      testServiceID,
+			ServiceVersion: *testVersion.Number,
+			WebpQuality:    &newWebpQuality,
+		})
+	})
+	if err == nil {
+		t.Fatalf("missing err")
+	}
+	expectedErr := "Webp quality must be less than or equal to 100"
+	if !strings.Contains(err.Error(), expectedErr) {
+		t.Fatalf("expected error to include '%s'; got: %v", expectedErr, err)
+	}
+}
+
+func TestClient_GetImageOptimizerDefaultSettings_validation(t *testing.T) {
+	var err error
+	_, err = testClient.GetImageOptimizerDefaultSettings(&GetImageOptimizerDefaultSettingsInput{
+		ServiceID:      "",
+		ServiceVersion: 3,
+	})
+	if err != ErrMissingServiceID {
+		t.Errorf("expected error %v; got: %v", ErrMissingServiceID, err)
+	}
+
+	_, err = testClient.GetImageOptimizerDefaultSettings(&GetImageOptimizerDefaultSettingsInput{
+		ServiceID:      "foo",
+		ServiceVersion: 0,
+	})
+	if err != ErrMissingServiceVersion {
+		t.Errorf("expected error %v; got: %v", ErrMissingServiceVersion, err)
+	}
+}
+
+func TestClient_UpdateImageOptimizerDefaultSettings_validation(t *testing.T) {
+	newUpscale := true
+
+	var err error
+	_, err = testClient.UpdateImageOptimizerDefaultSettings(&UpdateImageOptimizerDefaultSettingsInput{
+		ServiceID:      "",
+		ServiceVersion: 3,
+		Upscale:        &newUpscale,
+	})
+	if err != ErrMissingServiceID {
+		t.Errorf("expected error %v; got: %v", ErrMissingServiceID, err)
+	}
+
+	_, err = testClient.UpdateImageOptimizerDefaultSettings(&UpdateImageOptimizerDefaultSettingsInput{
+		ServiceID:      "foo",
+		ServiceVersion: 0,
+		Upscale:        &newUpscale,
+	})
+	if err != ErrMissingServiceVersion {
+		t.Errorf("expected error %v; got: %v", ErrMissingServiceVersion, err)
+	}
+
+	_, err = testClient.UpdateImageOptimizerDefaultSettings(&UpdateImageOptimizerDefaultSettingsInput{
+		ServiceID:      "foo",
+		ServiceVersion: 3,
+	})
+	if err != ErrMissingImageOptimizerDefaultSetting {
+		t.Errorf("expected error: %v, got: %v", ErrMissingImageOptimizerDefaultSetting, err)
+	}
+}
