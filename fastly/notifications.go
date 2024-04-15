458818c6ba7593e2a74090f57b75b768c97e249e
Darin Nee <darin.nee@fastly.com>
2024-04-15 15:08:42 -0700

    fix use of pointers per pr comments

--- fastly/notifications.go
+++ fastly/notifications.go
@@ -10,41 +10,41 @@ import (
 
 // Integration holds the configuration for one integration.
 type Integration struct {
-	CreatedAt   time.Time         `json:"created_at"`
-	Description string            `json:"description"`
-	Config      map[string]string `json:"config"`
-	ID          string            `json:"id"`
-	Name        string            `json:"name"`
-	Status      string            `json:"status"`
-	Type        string            `json:"type"`
-	UpdatedAt   time.Time         `json:"updated_at"`
+	CreatedAt   *time.Time         `json:"created_at"`
+	Description *string            `json:"description"`
+	Config      *map[string]string `json:"config"`
+	ID          *string            `json:"id"`
+	Name        *string            `json:"name"`
+	Status      *string            `json:"status"`
+	Type        *string            `json:"type"`
+	UpdatedAt   *time.Time         `json:"updated_at"`
 }
 
 // SearchIntegrationsInput is used as input to the SearchIntegrations function.
 type SearchIntegrationsInput struct {
-	// Cursor is the pagination cursor from a previous request's meta (optional).
+	// Cursor is the pagination cursor from a previous request's meta.
 	Cursor *string
-	// Limit is the maximum number of items included in each response (optional).
+	// Limit is the maximum number of items included in each response.
 	Limit *int
-	// Sort is the field on which to sort integrations (optional).
+	// Sort is the field on which to sort integrations.
 	Sort *string
-	// Type filters integrations by type (optional).
+	// Type filters integrations by type.
 	Type *string
 }
 
 // SearchIntegrationsResponse is the response for an integrations query.
 type SearchIntegrationsResponse struct {
-	Data []Integration    `json:"data"`
-	Meta IntegrationsMeta `json:"meta"`
+	Data *[]Integration    `json:"data"`
+	Meta *IntegrationsMeta `json:"meta"`
 }
 
 // IntegrationsMeeta holds metadata about an integrations query.
 type IntegrationsMeta struct {
-	Limit      int    `json:"limit"`
-	NextCursor string `json:"next_cursor"`
-	Sort       string `json:"sort"`
-	Total      int    `json:"total"`
-	Type       string `json:"type"`
+	Limit      *int    `json:"limit"`
+	NextCursor *string `json:"next_cursor"`
+	Sort       *string `json:"sort"`
+	Total      *int    `json:"total"`
+	Type       *string `json:"type"`
 }
 
 // SearchIntegrations retrieves filtered, paginated integrations.
@@ -84,12 +84,12 @@ func (c *Client) SearchIntegrations(i *SearchIntegrationsInput) (*SearchIntegrat
 // CreateIntegrationInput is used as input to the CreateIntegration function.
 type CreateIntegrationInput struct {
 	// Config is configuration specific to the integration type.
-	Config map[string]string
+	Config *map[string]string
 	// Description is the user submitted description of the integration.
 	Description *string
 	// Name is the user submitted name of the integration.
 	Name *string
-	// Type is the type of integration
+	// Type is the type of integration.
 	Type *string
 }
 
@@ -117,16 +117,16 @@ func (c *Client) CreateIntegration(i *CreateIntegrationInput) (*CreateIntegratio
 // GetIntegrationInput is used as input to the GetIntegration function.
 type GetIntegrationInput struct {
 	// ID of integration to fetch (required).
-	ID *string
+	ID string
 }
 
 // GetIntegration retrieves a specified integration.
 func (c *Client) GetIntegration(i *GetIntegrationInput) (*Integration, error) {
-	if i.ID == nil {
+	if i.ID == "" {
 		return nil, ErrMissingID
 	}
 
-	path := fmt.Sprintf("/notifications/integrations/%s", *i.ID)
+	path := fmt.Sprintf("/notifications/integrations/%s", i.ID)
 	resp, err := c.Get(path, nil)
 	if err != nil {
 		return nil, err
@@ -144,11 +144,11 @@ func (c *Client) GetIntegration(i *GetIntegrationInput) (*Integration, error) {
 // UpdateIntegrationInput is used as input to the UpdateIntegration function.
 type UpdateIntegrationInput struct {
 	// Config is configuration specific to the integration type.
-	Config map[string]string
+	Config *map[string]string
 	// Description is the user submitted description of the integration.
 	Description *string
 	// ID of integration to update (required).
-	ID *string
+	ID string
 	// Name is the user submitted name of the integration.
 	Name *string
 	// Type is the type of integration
@@ -157,11 +157,11 @@ type UpdateIntegrationInput struct {
 
 // UpdateIntegration updates the specified integration.
 func (c *Client) UpdateIntegration(i *UpdateIntegrationInput) error {
-	if i.ID == nil {
+	if i.ID == "" {
 		return ErrMissingID
 	}
 
-	path := fmt.Sprintf("/notifications/integrations/%s", *i.ID)
+	path := fmt.Sprintf("/notifications/integrations/%s", i.ID)
 	resp, err := c.PatchJSON(path, i, nil)
 	if err != nil {
 		return err
@@ -178,16 +178,16 @@ func (c *Client) UpdateIntegration(i *UpdateIntegrationInput) error {
 // DeleteIntegrationInput is used as input to the DeleteIntegration function.
 type DeleteIntegrationInput struct {
 	// ID of integration to delete (required).
-	ID *string
+	ID string
 }
 
 // DeleteIntegration deletes the specified integration.
 func (c *Client) DeleteIntegration(i *DeleteIntegrationInput) error {
-	if i.ID == nil {
+	if i.ID == "" {
 		return ErrMissingID
 	}
 
-	path := fmt.Sprintf("/notifications/integrations/%s", *i.ID)
+	path := fmt.Sprintf("/notifications/integrations/%s", i.ID)
 	resp, err := c.Delete(path, nil)
 	if err != nil {
 		return err
@@ -234,21 +234,21 @@ func (c *Client) GetIntegrationTypes() (*[]IntegrationType, error) {
 // GetWebhookSigningKeyInput is used as input to the GetWebhookSigningKey function.
 type GetWebhookSigningKeyInput struct {
 	// IntegrationID is the ID of the webhook integration which signing key to get (required).
-	IntegrationID *string
+	IntegrationID string
 }
 
 // WebhookSigningKeyResponse is the response for getting or rotating a webhook payload signing key.
 type WebhookSigningKeyResponse struct {
-	SigningKey string `json:"signingKey"`
+	SigningKey *string `json:"signingKey"`
 }
 
 // GetWebhookSigningKey retrieves the signing key for a webhook integration.
 func (c *Client) GetWebhookSigningKey(i *GetWebhookSigningKeyInput) (*WebhookSigningKeyResponse, error) {
-	if i.IntegrationID == nil {
+	if i.IntegrationID == "" {
 		return nil, ErrMissingIntegrationID
 	}
 
-	path := fmt.Sprintf("/notifications/integrations/%s/signingKey", *i.IntegrationID)
+	path := fmt.Sprintf("/notifications/integrations/%s/signingKey", i.IntegrationID)
 	resp, err := c.Get(path, nil)
 	if err != nil {
 		return nil, err
@@ -265,16 +265,16 @@ func (c *Client) GetWebhookSigningKey(i *GetWebhookSigningKeyInput) (*WebhookSig
 // RotateWebhookSigningKeyInput is used as input to the RotateWebhookSigningKey function.
 type RotateWebhookSigningKeyInput struct {
 	// IntegrationID is the ID of the webhook integration which signing key to rotate (required).
-	IntegrationID *string
+	IntegrationID string
 }
 
 // RotateWebhookSigningKey rotates the signing key for a webhook integration.
 func (c *Client) RotateWebhookSigningKey(i *RotateWebhookSigningKeyInput) (*WebhookSigningKeyResponse, error) {
-	if i.IntegrationID == nil {
+	if i.IntegrationID == "" {
 		return nil, ErrMissingIntegrationID
 	}
 
-	path := fmt.Sprintf("/notifications/integrations/%s/rotateSigningKey", *i.IntegrationID)
+	path := fmt.Sprintf("/notifications/integrations/%s/rotateSigningKey", i.IntegrationID)
 	resp, err := c.Post(path, nil)
 	if err != nil {
 		return nil, err
@@ -290,7 +290,7 @@ func (c *Client) RotateWebhookSigningKey(i *RotateWebhookSigningKeyInput) (*Webh
 
 // CreateMailinglistConfirmationInput is used as input to the CreateMailinglistConfirmation function.
 type CreateMailinglistConfirmationInput struct {
-	// Email is the mailinglist address (required).
+	// Email is the mailinglist address.
 	Email *string
 }
 
--- fastly/notifications_test.go
+++ fastly/notifications_test.go
@@ -21,7 +21,7 @@ func TestClient_Notifications(t *testing.T) {
 	}
 
 	cii := &CreateIntegrationInput{
-		Config: map[string]string{
+		Config: &map[string]string{
 			"address": "noreply@fastly.com",
 		},
 		Description: ToPointer("test description"),
@@ -38,11 +38,11 @@ func TestClient_Notifications(t *testing.T) {
 	defer func() {
 		record(t, "notifications/cleanup_integration", func(c *Client) {
 			err = c.DeleteIntegration(&DeleteIntegrationInput{
-				ID: cir.ID,
+				ID: *cir.ID,
 			})
 		})
 	}()
-	if *cir.ID == "" {
+	if cir.ID == nil {
 		t.Errorf("missing id")
 	}
 
@@ -59,19 +59,19 @@ func TestClient_Notifications(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if len(sir.Data) < 1 {
-		t.Errorf("bad data: %v", sir.Data)
+	if len(*sir.Data) < 1 {
+		t.Errorf("bad data: %v", *sir.Data)
 	}
-	if sir.Meta.Limit != 3 {
+	if *sir.Meta.Limit != 3 {
 		t.Errorf("bad meta limit: %v", sir.Meta.Limit)
 	}
-	if sir.Meta.Sort != "-created_at" {
+	if *sir.Meta.Sort != "-created_at" {
 		t.Errorf("bad meta sort, %v", sir.Meta.Sort)
 	}
-	if sir.Meta.Total < 1 {
+	if *sir.Meta.Total < 1 {
 		t.Errorf("bad meta total, %v", sir.Meta.Total)
 	}
-	if sir.Meta.Type != "mailinglist" {
+	if *sir.Meta.Type != "mailinglist" {
 		t.Errorf("bad meta type, %v", sir.Meta.Type)
 	}
 
@@ -79,7 +79,7 @@ func TestClient_Notifications(t *testing.T) {
 	var gi *Integration
 	record(t, "notifications/get_integration", func(c *Client) {
 		gi, err = c.GetIntegration(&GetIntegrationInput{
-			ID: cir.ID,
+			ID: *cir.ID,
 		})
 	})
 	if err != nil {
@@ -88,17 +88,17 @@ func TestClient_Notifications(t *testing.T) {
 	if gi.CreatedAt.IsZero() {
 		t.Errorf("missing created at")
 	}
-	if gi.Description != *cii.Description {
-		t.Errorf("bad description: %q (%q)", gi.Name, *cii.Name)
+	if *gi.Description != *cii.Description {
+		t.Errorf("bad description: %q (%q)", *gi.Description, *cii.Description)
 	}
-	if gi.ID != *cir.ID {
-		t.Errorf("bad id: %q (%q)", gi.ID, *cir.ID)
+	if *gi.ID != *cir.ID {
+		t.Errorf("bad id: %q (%q)", *gi.ID, *cir.ID)
 	}
-	if gi.Name != *cii.Name {
-		t.Errorf("bad name: %q (%q)", gi.Name, *cii.Name)
+	if *gi.Name != *cii.Name {
+		t.Errorf("bad name: %q (%q)", *gi.Name, *cii.Name)
 	}
-	if gi.Type != *cii.Type {
-		t.Errorf("bad type: %q (%q)", gi.Type, *cii.Type)
+	if *gi.Type != *cii.Type {
+		t.Errorf("bad type: %q (%q)", *gi.Type, *cii.Type)
 	}
 	if gi.UpdatedAt.IsZero() {
 		t.Errorf("missing updated at")
@@ -117,11 +117,11 @@ func TestClient_Notifications(t *testing.T) {
 	// Update integration
 	record(t, "notifications/update_integration", func(c *Client) {
 		err = c.UpdateIntegration(&UpdateIntegrationInput{
-			Config: map[string]string{
+			Config: &map[string]string{
 				"url": "https://foo.com/bar",
 			},
 			Description: ToPointer("test description updated"),
-			ID:          ToPointer(gi.ID),
+			ID:          *gi.ID,
 			Name:        ToPointer("test name updated"),
 			Type:        ToPointer("webhook"),
 		})
@@ -134,13 +134,13 @@ func TestClient_Notifications(t *testing.T) {
 	var rwskr *WebhookSigningKeyResponse
 	record(t, "notifications/rotate_webhook_signing_key", func(c *Client) {
 		rwskr, err = c.RotateWebhookSigningKey(&RotateWebhookSigningKeyInput{
-			IntegrationID: ToPointer(gi.ID),
+			IntegrationID: *gi.ID,
 		})
 	})
 	if err != nil {
 		t.Fatal(err)
 	}
-	if rwskr.SigningKey == "" {
+	if rwskr.SigningKey == nil {
 		t.Errorf("rotate missing signing key")
 	}
 
@@ -148,20 +148,20 @@ func TestClient_Notifications(t *testing.T) {
 	var gwskr *WebhookSigningKeyResponse
 	record(t, "notifications/get_webhook_signing_key", func(c *Client) {
 		gwskr, err = c.GetWebhookSigningKey(&GetWebhookSigningKeyInput{
-			IntegrationID: ToPointer(gi.ID),
+			IntegrationID: *gi.ID,
 		})
 	})
 	if err != nil {
 		t.Fatal(err)
 	}
-	if gwskr.SigningKey == "" {
+	if gwskr.SigningKey == nil {
 		t.Errorf("get missing signing key")
 	}
 
 	// Delete integration
 	record(t, "notifications/delete_integration", func(c *Client) {
 		err = c.DeleteIntegration(&DeleteIntegrationInput{
-			ID: ToPointer(gi.ID),
+			ID: *gi.ID,
 		})
 	})
 	if err != nil {
@@ -170,45 +170,35 @@ func TestClient_Notifications(t *testing.T) {
 }
 
 func TestClient_GetIntegration_validation(t *testing.T) {
-	_, err := testClient.GetIntegration(&GetIntegrationInput{
-		ID: nil,
-	})
+	_, err := testClient.GetIntegration(&GetIntegrationInput{})
 	if err != ErrMissingID {
 		t.Errorf("bad error: %s", err)
 	}
 }
 
 func TestClient_UpdateIntegration_validation(t *testing.T) {
-	err := testClient.UpdateIntegration(&UpdateIntegrationInput{
-		ID: nil,
-	})
+	err := testClient.UpdateIntegration(&UpdateIntegrationInput{})
 	if err != ErrMissingID {
 		t.Errorf("bad error: %s", err)
 	}
 }
 
 func TestClient_DeleteIntegration_validation(t *testing.T) {
-	err := testClient.DeleteIntegration(&DeleteIntegrationInput{
-		ID: nil,
-	})
+	err := testClient.DeleteIntegration(&DeleteIntegrationInput{})
 	if err != ErrMissingID {
 		t.Errorf("bad error: %s", err)
 	}
 }
 
 func TestClient_GetWebhookSigningKey_validation(t *testing.T) {
-	_, err := testClient.GetWebhookSigningKey(&GetWebhookSigningKeyInput{
-		IntegrationID: nil,
-	})
+	_, err := testClient.GetWebhookSigningKey(&GetWebhookSigningKeyInput{})
 	if err != ErrMissingIntegrationID {
 		t.Errorf("bad error: %s", err)
 	}
 }
 
 func TestClient_RotateWebhookSigningKey_validation(t *testing.T) {
-	_, err := testClient.RotateWebhookSigningKey(&RotateWebhookSigningKeyInput{
-		IntegrationID: nil,
-	})
+	_, err := testClient.RotateWebhookSigningKey(&RotateWebhookSigningKeyInput{})
 	if err != ErrMissingIntegrationID {
 		t.Errorf("bad error: %s", err)
 	}
