a173780363b47ffa25aca73cd689b41bccae4a8e
Brooks Cunningham <Brooks.Cunningham@gmail.com>
2024-08-20 12:52:54 -0500

    feat(product_enablement): Add support for the 'bot_management' product. (#539)
    * Adding ProductBotManagement product enablement
    
    * Update product_enablement_bot_management_test.go
    
    * Adding and updating tests

--- fastly/product_enablement.go
+++ fastly/product_enablement.go
@@ -20,6 +20,8 @@ type Product int64
 
 func (p Product) String() string {
 	switch p {
+	case ProductBotManagement:
+		return "bot_management"
 	case ProductBrotliCompression:
 		return "brotli_compression"
 	case ProductDomainInspector:
@@ -40,6 +42,7 @@ func (p Product) String() string {
 
 const (
 	ProductUndefined Product = iota
+	ProductBotManagement
 	ProductBrotliCompression
 	ProductDomainInspector
 	ProductFanout
