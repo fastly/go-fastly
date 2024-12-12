de962ad74029230ddc2efc5405ebe6b2a7c18e34
Kevin P. Fleming <kevin.fleming@fastly.com>
2024-12-12 14:04:32 -0500

    Apply review feedback.

--- internal/products/api_base.go
+++
@@ -1,6 +0,0 @@
-package products
-
-// NullInput is used to indicate to the generic FunctionalTest
-// constructors that the operation being tested does not accept an
-// Input struct
-type NullInput struct{}
--- internal/products/types.go
+++ internal/products/types.go
@@ -13,3 +13,8 @@ type ProductOutput interface {
 	ProductID() *string
 	ServiceID() *string
 }
+
+// NullInput is used to indicate to the generic FunctionalTest
+// constructors that the operation being tested does not accept an
+// Input struct
+type NullInput struct{}
