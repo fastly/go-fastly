37b0997491e44cbac38dedd4a7f63dac1e076d3c
Josh Woodward <josh.woodward2693@gmail.com>
2025-02-11 10:03:11 -0500

    Add field to UpdateCustomTLSCertificateInput struct

--- fastly/tls_custom_certificate.go
+++ fastly/tls_custom_certificate.go
@@ -182,6 +182,11 @@ type UpdateCustomTLSCertificateInput struct {
 	ID string `jsonapi:"primary,tls_certificate"`
 	// Name is a customizable name for your certificate.
 	Name string `jsonapi:"attr,name,omitempty"`
+	// AllowUntrustedRoot enables the upload of a certificate signed by a self-signing CA as the root.
+	// The main use case for enabling this setting is to enable a fully localized development setup where you are generating your own certificates.
+	// In most cases, this setting should never be turned on since it makes it possible for the uploading of conflicting certificates that may interfere with production traffic.
+	// If you are going to enable this feature make sure to take proper precaution and only upload self-signed certificates that certify "dummy" domains that do not overlap with the actual domains you own.
+	AllowUntrustedRoot bool `jsonapi:"attr,allow_untrusted_root,omitempty"`
 }
 
 // UpdateCustomTLSCertificate updates the specified resource.
