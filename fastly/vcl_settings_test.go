f5d028b35cd97e694c877cf02f8188cc483ce13f
Anthony Gomez <anthony.gomez@fastly.com>
2025-05-01 12:35:11 -0400

    fix vcl_settings test

--- fastly/vcl_settings_test.go
+++ fastly/vcl_settings_test.go
@@ -69,9 +69,11 @@ func TestClient_UpdateSettingsInput_default_ttl(t *testing.T) {
 	if err != nil {
 		t.Errorf("Unexpected error: %v", err)
 	}
+	//remove Context like we do in the code
+	v.Del("Context")
 	body := v.Encode()
 
-	if body != "Context=&ServiceID=foo&ServiceVersion=1&general.default_ttl=0" {
+	if body != "ServiceID=foo&ServiceVersion=1&general.default_ttl=0" {
 		t.Errorf("Update request should contain a default_ttl. Got: %s", body)
 	}
 }
