2edb8ed39b678b007438c7899c48e4829bec0503
Integralist <integralist@fastly.com>
2025-05-19 10:47:39 +0100

    feat(domains): add description field

--- fastly/domains/v1/api_create.go
+++ fastly/domains/v1/api_create.go
@@ -13,6 +13,8 @@ import (
 type CreateInput struct {
 	// Context, if supplied, will be used as the Request's context.
 	Context *context.Context `json:"-"`
+	// Description is the description for the domain.
+	Description *string `json:"description"`
 	// FQDN is the fully-qualified domain name of the domain (required).
 	FQDN *string `json:"fqdn"`
 	// ServiceID is the service_id associated with the domain or nil if there
