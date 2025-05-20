94e660674069583533a8d8975d6ebf1c5ede0406
Richard Carillo <rcarillo@fastly.com>
2025-05-20 10:35:04 -0400

    Struct Updates
    (Still working on test cases)

--- fastly/ngwaf/v1/virtualpatches/api_response.go
+++ fastly/ngwaf/v1/virtualpatches/api_response.go
@@ -12,20 +12,20 @@ type MetaVirtualPatch struct {
 
 // VirtualPatch is the API response structure for the list of virtial patch operations.
 type VirtualPatch struct {
-	// ID is the workspace VirtualPatch (UUID).
-	ID string `json:"id"`
-	// Mode is the mode of the VirtualPatch.
+	// Enabled is the toggle status indicator of the VirtualPatch.
 	Enabled string `json:"enabled"`
-	// WorkspaceID is the workspace identifier (UUID).
+	// Description is the description of the workspace.
+	Description string `json:"description"`
+	// ID is the virtual patch identifier.
+	ID string `json:"id"`
+	// Mode is action to take when a signal for virtual patch is detected.
 	Mode string `json:"mode"`
 	// WorkspaceID is the workspace identifier (UUID).
 	WorkspaceID string `json:"workspace_id"`
-	// Description is the description of the workspace.
-	Description string `json:"description"`
 	// CreatedAt is the date and time in ISO 8601 format.
 	CreatedAt time.Time `json:"created_at"`
-	// UpdatedAt is the date and time in ISO 8601 format.
-	UpdatedAt time.Time `json:"updated_at"`
 	// DeletedAt is the date and time in ISO 8601 format.
 	DeletedAt time.Time `json:"deleted_at"`
+	// UpdatedAt is the date and time in ISO 8601 format.
+	UpdatedAt time.Time `json:"updated_at"`
 }
