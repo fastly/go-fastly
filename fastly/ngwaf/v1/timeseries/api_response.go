3955569c2a1ca4321b5f24f04c143044cacfb39a
Richard Carillo <rcarillo@fastly.com>
2026-06-15 11:06:40 -0400

    lint corrections

--- fastly/ngwaf/v1/timeseries/api_response.go
+++ fastly/ngwaf/v1/timeseries/api_response.go
@@ -1,15 +1,15 @@
 package timeseries
 
-// TimeseriesDataPoint is a single data point in the timeseries.
-type TimeseriesDataPoint struct {
+// DataPoint is a single data point in the timeseries.
+type DataPoint struct {
 	// Dimensions are the dimension values for this data point.
-	Dimensions TimeseriesDimensions `json:"dimensions"`
+	Dimensions Dimensions `json:"dimensions"`
 	// Values are the metric values for this data point.
 	Values []map[string]any `json:"values"`
 }
 
-// TimeseriesDimensions holds the grouping dimensions for a data point.
-type TimeseriesDimensions struct {
+// Dimensions holds the grouping dimensions for a data point.
+type Dimensions struct {
 	// Time is the timestamp for this data point.
 	Time string `json:"time"`
 	// Workspace is the workspace identifier.
@@ -19,7 +19,7 @@ type TimeseriesDimensions struct {
 // Timeseries is the API response structure for the list timeseries operation.
 type Timeseries struct {
 	// Data is the list of returned timeseries data points.
-	Data []TimeseriesDataPoint `json:"data"`
+	Data []DataPoint `json:"data"`
 	// Meta is the information for total timeseries.
 	Meta MetaTimeseries `json:"meta"`
 }
