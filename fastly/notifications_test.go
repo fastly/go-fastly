b94db46d1686a4c54b7c4bec9ca01fce32e137dd
Anthony Gomez <anthony.gomez@fastly.com>
2026-07-29 12:46:45 -0400

    [CDTOOL-1689] Add typed helpers for new integration types (#842)
    ### Change summary
    
    Add IntegrationType* constants and per-vendor *Config helper structs
    (with ToMap()) for the Datadog, Jira Issue, Jira Service Management,
    OpsGenie, and Splunk On-Call integration types now supported by the
    Notification Service API.
    
    [API
    Docs](https://www.fastly.com/documentation/reference/api/observability/notifications/)
    
     All Submissions:
    
    * [x] Have you followed the guidelines in our Contributing document?
    * [x] Have you checked to ensure there aren't other open [Pull
    Requests](https://github.com/fastly/go-fastly/pulls) for the same
    update/change?
    
    ### New Feature Submissions:
    
    * [x] Does your submission pass tests?

--- fastly/notifications_test.go
+++ fastly/notifications_test.go
@@ -3,6 +3,7 @@ package fastly
 import (
 	"context"
 	"errors"
+	"reflect"
 	"testing"
 )
 
@@ -120,7 +121,7 @@ func TestClient_Notifications(t *testing.T) {
 	Record(t, "notifications/update_integration", func(c *Client) {
 		err = c.UpdateIntegration(context.TODO(), &UpdateIntegrationInput{
 			Config: map[string]string{
-				"url": "https://foo.com/bar",
+				"webhook": "https://foo.com/bar",
 			},
 			Description: ToPointer("test description updated"),
 			ID:          *gi.ID,
@@ -169,6 +170,92 @@ func TestClient_Notifications(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	// Create, get, and delete an integration for each of the newer
+	// integration types, using their typed config helpers.
+	newTypeIntegrations := []struct {
+		fixture string
+		typ     string
+		config  map[string]string
+	}{
+		{
+			fixture: "datadog",
+			typ:     IntegrationTypeDatadog,
+			config:  DatadogConfig{APIKey: "abc123def456", Site: "datadoghq.eu"}.ToMap(),
+		},
+		{
+			fixture: "jiraissue",
+			typ:     IntegrationTypeJiraIssue,
+			config: JiraIssueConfig{ //nolint: gosec
+				BaseURL:    "https://your-org.atlassian.net",
+				Username:   "user@example.com",
+				Token:      "AbCdEfGhIjKlMnOpQrStUvWx",
+				ProjectKey: "PROJ",
+				IssueType:  "Bug",
+			}.ToMap(),
+		},
+		{
+			fixture: "jsm",
+			typ:     IntegrationTypeJSM,
+			config:  JSMConfig{APIKey: "g4ff854d-a14c-46a8-b8f0-0960774319dd"}.ToMap(), //nolint: gosec
+		},
+		{
+			fixture: "opsgenie",
+			typ:     IntegrationTypeOpsGenie,
+			config:  OpsGenieConfig{APIKey: "a1bb854d-b24c-46a8-c9f0-1960774319ee"}.ToMap(), //nolint: gosec
+		},
+		{
+			fixture: "splunkoncall",
+			typ:     IntegrationTypeSplunkOnCall,
+			config:  SplunkOnCallConfig{URL: "https://alert.victorops.com/integrations/generic/20131114/alert/XXXX"}.ToMap(),
+		},
+	}
+
+	for _, nti := range newTypeIntegrations {
+		// Create integration
+		var newCir *CreateIntegrationResponse
+		Record(t, "notifications/create_"+nti.fixture+"_integration", func(c *Client) {
+			newCir, err = c.CreateIntegration(context.TODO(), &CreateIntegrationInput{
+				Config:      nti.config,
+				Description: ToPointer("test " + nti.fixture + " description"),
+				Name:        ToPointer("test " + nti.fixture + " name"),
+				Type:        ToPointer(nti.typ),
+			})
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		if newCir == nil || newCir.ID == nil {
+			t.Fatalf("missing response or integration id for %s integration", nti.fixture)
+		}
+
+		// Get integration
+		var newGi *Integration
+		Record(t, "notifications/get_"+nti.fixture+"_integration", func(c *Client) {
+			newGi, err = c.GetIntegration(context.TODO(), &GetIntegrationInput{
+				ID: *newCir.ID,
+			})
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		if *newGi.Type != nti.typ {
+			t.Errorf("bad type for %s integration: %q", nti.fixture, *newGi.Type)
+		}
+		if *newGi.ID != *newCir.ID {
+			t.Errorf("bad id for %s integration: %q (%q)", nti.fixture, *newGi.ID, *newCir.ID)
+		}
+
+		// Delete integration
+		Record(t, "notifications/delete_"+nti.fixture+"_integration", func(c *Client) {
+			err = c.DeleteIntegration(context.TODO(), &DeleteIntegrationInput{
+				ID: *newCir.ID,
+			})
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
 }
 
 func TestClient_GetIntegration_validation(t *testing.T) {
@@ -189,6 +276,65 @@ func TestClient_DeleteIntegration_validation(t *testing.T) {
 	err := TestClient.DeleteIntegration(context.TODO(), &DeleteIntegrationInput{})
 	if !errors.Is(err, ErrMissingID) {
 		t.Errorf("bad error: %s", err)
+	}
+}
+
+func TestDatadogConfig_ToMap(t *testing.T) {
+	got := DatadogConfig{APIKey: "abc123", Site: "datadoghq.eu"}.ToMap()
+	want := map[string]string{"apikey": "abc123", "site": "datadoghq.eu"}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("bad map: got %v, want %v", got, want)
+	}
+
+	// Site is optional and should be omitted when empty.
+	got = DatadogConfig{APIKey: "abc123"}.ToMap()
+	want = map[string]string{"apikey": "abc123"}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("bad map: got %v, want %v", got, want)
+	}
+}
+
+func TestJiraIssueConfig_ToMap(t *testing.T) {
+	got := JiraIssueConfig{ //nolint: gosec
+		BaseURL:    "https://your-org.atlassian.net",
+		Username:   "user@example.com",
+		Token:      "AbCdEfGhIjKlMnOpQrStUvWx",
+		ProjectKey: "PROJ",
+		IssueType:  "Bug",
+	}.ToMap()
+	want := map[string]string{ //nolint: gosec
+		"baseurl":    "https://your-org.atlassian.net",
+		"username":   "user@example.com",
+		"token":      "AbCdEfGhIjKlMnOpQrStUvWx",
+		"projectkey": "PROJ",
+		"issuetype":  "Bug",
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("bad map: got %v, want %v", got, want)
+	}
+}
+
+func TestJSMConfig_ToMap(t *testing.T) {
+	got := JSMConfig{APIKey: "g4ff854d-a14c-46a8-b8f0-0960774319dd"}.ToMap()    //nolint: gosec
+	want := map[string]string{"apikey": "g4ff854d-a14c-46a8-b8f0-0960774319dd"} //nolint: gosec
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("bad map: got %v, want %v", got, want)
+	}
+}
+
+func TestOpsGenieConfig_ToMap(t *testing.T) {
+	got := OpsGenieConfig{APIKey: "a1bb854d-b24c-46a8-c9f0-1960774319ee"}.ToMap() //nolint: gosec
+	want := map[string]string{"apikey": "a1bb854d-b24c-46a8-c9f0-1960774319ee"}   //nolint: gosec
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("bad map: got %v, want %v", got, want)
+	}
+}
+
+func TestSplunkOnCallConfig_ToMap(t *testing.T) {
+	got := SplunkOnCallConfig{URL: "https://alert.victorops.com/integrations/generic/20131114/alert/XXXX"}.ToMap()
+	want := map[string]string{"url": "https://alert.victorops.com/integrations/generic/20131114/alert/XXXX"}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("bad map: got %v, want %v", got, want)
 	}
 }
 
